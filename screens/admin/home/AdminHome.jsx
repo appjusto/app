1500f2525a79becc6d254a4278e9fec6c057f803
Pedro Andrade <pdandradeb@gmail.com>
2020-07-06 18:21:42 -0300

    Showing notification in UI

--- screens/admin/home/AdminHome.jsx
+++ screens/admin/home/AdminHome.jsx
@@ -1,13 +1,13 @@
-import React, { useEffect, useContext } from 'react';
+import React, { useEffect, useContext, useState } from 'react';
 import { StyleSheet, View, Dimensions, Text } from 'react-native';
 import { Marker } from 'react-native-maps';
 import { useDispatch, useSelector } from 'react-redux';
+import * as Notifications from 'expo-notifications';
 
 import { ApiContext } from '../../../store/api';
 import { fetchVisibleCouriers } from '../../../store/actions/courier';
 import { getVisibleCouriers } from '../../../store/selectors/courier';
 import DefaultMap from '../../common/DefaultMap';
-
 
 const defaultDeltas = {
   latitudeDelta: 0.0250,
@@ -21,12 +21,21 @@ export default function App({ token }) {
 
   // state
   const visibleCouriers = useSelector(getVisibleCouriers);
-  console.log(visibleCouriers);
+  const [notification, setNotification] = useState(null);
 
   // side effects
   useEffect(() => {
     dispatch(fetchVisibleCouriers(api));
   }, [])
+
+  useEffect(() => {
+    Notifications.addNotificationReceivedListener((n) => {
+      setNotification(n);
+    });
+    return () => {
+      Notifications.removeAllNotificationListeners();
+    };
+  }, []);
 
   // UI
   const renderMap = () => {
@@ -35,6 +44,9 @@ export default function App({ token }) {
     return (
       <View style={{ flex: 1 }}>
         <Text>Token: {token}</Text>
+        <Text>Title: {notification && notification.request.content.title} </Text>
+        <Text>Body: {notification && notification.request.content.body}</Text>
+        <Text>Data: {notification && JSON.stringify(notification.request.content.data.body)}</Text>
         <DefaultMap style={[styles.map, { width }]}>
           {visibleCouriers.map((courier) => (
             <Marker key={courier.id} coordinate={courier.lastKnownLocation} />
