5ab04d4efaed006cbe7fb7abcd16a9161bb67adf
Pedro Andrade <pdandradeb@gmail.com>
2020-08-03 14:45:45 -0300

    pathPrefix bugfix wannabe (2nd try)

--- screens/app/context.tsx
+++ screens/app/context.tsx
@@ -1,6 +1,8 @@
 import { NavigationContainer } from '@react-navigation/native';
+import Constants from 'expo-constants';
 import * as Linking from 'expo-linking';
 import React, { ReactNode } from 'react';
+import { Platform, ToastAndroid } from 'react-native';
 import { Provider } from 'react-redux';
 
 import { createStore } from '../../store';
@@ -25,8 +27,13 @@ export interface Props {
 export const AppContext = ({ children }: Props) => {
   const path = extra.flavor;
   const linking = {
-    prefixes: [Linking.makeUrl(path), `https://link.appjusto.com.br/${path}`],
+    prefixes: [Linking.makeUrl(path), `https://link.appjusto.com.br/app/${path}`],
   };
+
+  const log = { l: linking, r: Constants.manifest.revisionId ?? '' };
+  if (Platform.OS === 'android') {
+    ToastAndroid.show(JSON.stringify(log), ToastAndroid.LONG);
+  }
 
   // UI
   return (
