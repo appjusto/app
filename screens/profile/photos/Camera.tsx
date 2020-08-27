e937561d291e5945a21e723cd2a449b6290d15fc
Daniel Lima <danielfbl@gmail.com>
2020-08-27 17:30:45 -0300

    Added a button to flip the camera

--- screens/profile/photos/Camera.tsx
+++ screens/profile/photos/Camera.tsx
@@ -1,11 +1,18 @@
 import { Camera } from 'expo-camera';
 import React, { useState, useEffect } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, TouchableOpacity } from 'react-native';
 
 export default function () {
   // state
   const [type, setType] = useState(Camera.Constants.Type.back);
   const [hasPermission, setHasPermission] = useState(null);
+
+  // handlers
+  function switchCameraHandler() {
+    setType(
+      type === Camera.Constants.Type.back ? Camera.Constants.Type.front : Camera.Constants.Type.back
+    );
+  }
 
   // requests permission as soon as the user accesses the screen
   useEffect(() => {
@@ -25,7 +32,16 @@ export default function () {
   //UI
   return (
     <View style={{ flex: 1 }}>
-      <Camera style={{ flex: 1 }} type={type} />
+      <Camera style={{ flex: 1 }} type={type}>
+        <View style={{ flex: 1, backgroundColor: 'transparent', flexDirection: 'row' }}>
+          <TouchableOpacity
+            style={{ position: 'absolute', bottom: 16, left: 16 }}
+            onPress={switchCameraHandler}
+          >
+            <Text style={{ fontSize: 16, color: 'white' }}>Flip camera</Text>
+          </TouchableOpacity>
+        </View>
+      </Camera>
     </View>
   );
 }
