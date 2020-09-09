a2b1a870b34627fc266ef8ce6f37c48181edb4db
Pedro Andrade <pdandradeb@gmail.com>
2020-09-09 14:48:23 -0300

    Function to upload both profile images

--- common/store/courier/actions.ts
+++ common/store/courier/actions.ts
@@ -29,6 +29,42 @@ export const uploadDocumentImage = (api: Api) => (
   return api.files().upload(documentPath, documentUri, progressHandler);
 };
 
+export const uploadProfileImages = (api: Api) => (
+  courierId: string,
+  selfieUri?: string,
+  documentUri?: string,
+  progressHandler?: (progress: number) => void
+) => async (dispatch: AppDispatch) => {
+  let selfieProgress = 0;
+  let documentProgress = 0;
+  let totalProgress = 0;
+
+  const updateProgress = () => {
+    if (progressHandler) {
+      totalProgress = Math.floor((selfieProgress + documentProgress) / 2);
+      progressHandler(totalProgress);
+    }
+  };
+
+  if (!selfieUri) selfieProgress = 100;
+  else {
+    const selfiePath = `couriers/${courierId}/selfie.jpg`;
+    return api.files().upload(selfiePath, selfieUri, (progress: number) => {
+      selfieProgress = progress;
+      updateProgress();
+    });
+  }
+
+  if (!documentUri) documentProgress = 100;
+  else {
+    const documentPath = `couriers/${courierId}/document.jpg`;
+    return api.files().upload(documentPath, documentUri, (progress: number) => {
+      documentProgress = progress;
+      updateProgress();
+    });
+  }
+};
+
 export const getSelfieURL = (api: Api) => (courierId: string) => async (dispatch: AppDispatch) => {
   const selfiePath = `couriers/${courierId}/selfie_100x100.jpg`;
   return api.files().getDownloadURL(selfiePath);
