a5ec6cd10d1a1b5562385153b5e9421ff169cdc9
Daniel Lima <danielfbl@gmail.com>
2020-09-30 15:53:32 -0300

    new layout for the CreateFleet screen

--- courier/approved/profile/fleet/CreateFleet.tsx
+++ courier/approved/profile/fleet/CreateFleet.tsx
@@ -16,7 +16,7 @@ import { getCourier } from '../../../../common/store/courier/selectors';
 import { createFleet } from '../../../../common/store/fleet/actions';
 import { getUIBusy } from '../../../../common/store/ui/selectors';
 import { texts, screens, colors, padding } from '../../../../common/styles';
-import { formatCurrency, formatDistance } from '../../../../common/utils/formatters';
+import { formatCurrency, formatDistance, formatPct } from '../../../../common/utils/formatters';
 import { t } from '../../../../strings';
 import { FleetParamList } from './types';
 
@@ -61,6 +61,8 @@ export default function ({ navigation, route }: Props) {
   const [additionalPerKmAfterThreshold, setAdditionalPerKmAfterThreshold] = useState(100);
   const [maxDistance, setMaxDistance] = useState(10000);
   const [maxDistanceToOrigin, setMaxDistanceToOrigin] = useState(3000);
+  const [valueThreshold, setValueThreshold] = useState(100);
+  const [feePctOverValue, setFeePcTOverValue] = useState(0);
   const canSubmit = useMemo(() => {
     return !isEmpty(name) && !isEmpty(description);
   }, [name, description]);
@@ -81,8 +83,8 @@ export default function ({ navigation, route }: Props) {
         additionalPerKmAfterThreshold,
         maxDistance,
         maxDistanceToOrigin,
-        valueThreshold: 0,
-        feePctOverValue: 0,
+        valueThreshold,
+        feePctOverValue,
         situation: 'approved',
         createdBy: courier.id,
       })
@@ -90,131 +92,318 @@ export default function ({ navigation, route }: Props) {
     navigation.goBack();
   };
 
+  // replace all the sliders with the new counter
+
   // UI
   return (
     <ScrollView>
-      <PaddedView style={{ ...screens.configScreen }}>
-        <Text style={{ ...texts.default, marginTop: 8, color: colors.darkGrey }}>
-          {t('Sua frota ficará ativa depois que pelo menos 10 entregadores participarem dela.')}
-        </Text>
-        <DefaultInput
-          ref={nameRef}
-          title={t('Nome')}
-          placeholder={t('Nome com no máximo 30 caracteres')}
-          value={name}
-          returnKeyType="next"
-          blurOnSubmit={false}
-          maxLength={30}
-          onChangeText={setName}
-          onSubmitEditing={() => descriptionRef.current?.focus()}
-          keyboardType="default"
-        />
-        <DefaultInput
-          ref={descriptionRef}
-          title={t('Descrição')}
-          placeholder={t('Descreva sua frota em até 280 caracteres')}
-          value={description}
-          returnKeyType="next"
-          blurOnSubmit={false}
-          maxLength={280}
-          numberOfLines={3}
-          onChangeText={setDescription}
-          keyboardType="default"
-        />
-        <View>
-          <View style={{ marginTop: padding }}>
-            <Text style={[texts.big]}>{t('Tarifa base')}</Text>
-            <Text style={{ ...texts.default, marginTop: 8, color: colors.darkGrey }}>
+      <View style={{ ...screens.configScreen }}>
+        <PaddedView>
+          <Text style={{ ...texts.big }}>{t('Criar nova frota')}</Text>
+          <Text style={{ ...texts.default, color: colors.darkGrey, marginVertical: 8 }}>
+            {t('Preencha as informações da sua frota')}
+          </Text>
+          <DefaultInput
+            ref={nameRef}
+            title={t('Nome')}
+            placeholder={t('Nome da frota em até 36 caracteres')}
+            value={name}
+            returnKeyType="next"
+            blurOnSubmit={false}
+            maxLength={36}
+            onChangeText={setName}
+            onSubmitEditing={() => descriptionRef.current?.focus()}
+            keyboardType="default"
+            style={{ marginBottom: 4 }}
+          />
+          {/* add logic to the counter below */}
+          <Text style={{ ...texts.small, color: colors.darkGrey, marginBottom: padding }}>
+            0/36 {t('caracteres')}
+          </Text>
+          <DefaultInput
+            ref={descriptionRef}
+            title={t('Descrição')}
+            placeholder={t('Descreva sua frota em até 140 caracteres')}
+            value={description}
+            returnKeyType="next"
+            blurOnSubmit={false}
+            maxLength={140}
+            numberOfLines={3}
+            onChangeText={setDescription}
+            keyboardType="default"
+            style={{ marginBottom: 4 }}
+          />
+          {/* add logic to the counter below */}
+          <Text style={{ ...texts.small, color: colors.darkGrey, marginBottom: padding }}>
+            0/140 {t('caracteres')}
+          </Text>
+          <View>
+            <View
+              style={{
+                flexDirection: 'row',
+                alignItems: 'center',
+                marginTop: padding,
+                justifyContent: 'space-between',
+                marginBottom: 8,
+              }}
+            >
+              <Text style={{ ...texts.medium }}>{t('Pagamento Mínimo')}</Text>
+              <RoundedText>{formatCurrency(minimumFee)}</RoundedText>
+            </View>
+            <Text style={{ ...texts.default, color: colors.darkGrey }}>
               {t(
-                'A tarifa base é composta por um valor mínimo e uma distância de atendimento. A partir dessa distância, você ganhará um adicional por km ou fração.'
+                'Defina o valor que os entregadores dessa frota receberão ao percorrer a Distância Inicial Mínima.'
               )}
             </Text>
-          </View>
-          <View style={{ flexDirection: 'row', alignItems: 'center', marginTop: padding }}>
-            <Text style={{ ...texts.medium }}>{t('Distância base: ')}</Text>
-            <RoundedText>{formatDistance(distanceThreshold)}</RoundedText>
-          </View>
-          <CustomSlider
-            value={distanceThreshold}
-            maximumValue={5000}
-            step={500}
-            onValueChange={setDistanceThreshold}
-          />
-          <View
-            style={{
-              flexDirection: 'row',
-              alignItems: 'center',
-              flexWrap: 'wrap',
-              marginTop: padding,
-            }}
-          >
-            <Text style={{ ...texts.medium }}>{t('Valor para corridas até')} </Text>
-            <RoundedText>{`${formatDistance(distanceThreshold)}`}</RoundedText>
-            <Text style={{ ...texts.medium }}>{t(' é de ')}</Text>
-            <RoundedText>{`${formatCurrency(minimumFee)}`}</RoundedText>
-          </View>
-          <CustomSlider
-            value={minimumFee}
-            minimumValue={100}
-            maximumValue={3000}
-            step={50}
-            onValueChange={setMinimumFee}
-          />
-          <View
-            style={{
-              flexDirection: 'row',
-              alignItems: 'center',
-              flexWrap: 'wrap',
-              marginTop: padding,
-            }}
-          >
-            <Text style={{ ...texts.medium }}>{`${t('A partir de')} ${formatDistance(
-              distanceThreshold
-            )} ${t(', o valor adicional por km é de:')}`}</Text>
-            <RoundedText>{`${formatCurrency(additionalPerKmAfterThreshold)}`}</RoundedText>
-          </View>
-          <CustomSlider
-            value={additionalPerKmAfterThreshold}
-            minimumValue={100}
-            maximumValue={3000}
-            step={50}
-            onValueChange={setAdditionalPerKmAfterThreshold}
-          />
-          <View style={{ marginTop: padding }}>
-            <Text style={[texts.big]}>{t('Limites')}</Text>
-            <Text style={{ ...texts.default, marginTop: 8, color: colors.darkGrey }}>
-              {t('Defina as distâncias máximas até o ponto de coleta e do trajeto.')}
+            <CustomSlider
+              value={minimumFee}
+              maximumValue={5000}
+              step={500}
+              onValueChange={setMinimumFee}
+            />
+            <View
+              style={{
+                flexDirection: 'row',
+                alignItems: 'center',
+                marginTop: padding,
+                justifyContent: 'space-between',
+                marginBottom: 8,
+              }}
+            >
+              <Text style={{ ...texts.medium }}>{t('Distância Inicial Mínima')} </Text>
+              <RoundedText>{`${formatDistance(distanceThreshold)}`}</RoundedText>
+            </View>
+            <Text style={{ ...texts.default, color: colors.darkGrey }}>
+              {t(
+                'Defina em Km a distância para o Pagamento Mínimo. Abaixo dessa distância, os entregadores dessa frota receberão o Pagamento Mínimo. Acima dessa distância, os entregadores receberão um Valor Adicional por Km Rodado.'
+              )}
             </Text>
+            <CustomSlider
+              value={distanceThreshold}
+              minimumValue={100}
+              maximumValue={3000}
+              step={50}
+              onValueChange={setDistanceThreshold}
+            />
+            <View
+              style={{
+                flexDirection: 'row',
+                alignItems: 'center',
+                marginTop: padding,
+                justifyContent: 'space-between',
+                marginBottom: 8,
+              }}
+            >
+              <Text style={{ ...texts.medium }}>{t('Valor Adicional por Km Rodado')}</Text>
+              <RoundedText>{`${formatCurrency(additionalPerKmAfterThreshold)}`}</RoundedText>
+            </View>
+            <Text style={{ ...texts.default, color: colors.darkGrey }}>
+              {t(
+                'Defina o valor adicional que os entregadores dessa frota receberão por Km ao percorrer uma distância acima da Distância Inicial Mínima.'
+              )}
+            </Text>
+            <CustomSlider
+              value={additionalPerKmAfterThreshold}
+              minimumValue={100}
+              maximumValue={3000}
+              step={50}
+              onValueChange={setAdditionalPerKmAfterThreshold}
+            />
+            <View
+              style={{
+                flexDirection: 'row',
+                alignItems: 'center',
+                marginTop: padding,
+                justifyContent: 'space-between',
+                marginBottom: 8,
+              }}
+            >
+              <Text style={{ ...texts.medium }}>{t('Distância Máxima para Entrega')}</Text>
+              <RoundedText>{formatDistance(maxDistance)}</RoundedText>
+            </View>
+            <Text style={{ ...texts.default, color: colors.darkGrey }}>
+              {t(
+                'Defina em Km a distância máxima que os entregadores dessa frota poderão percorrer para fazer uma entrega. Pedidos recebidos com distância máxima acima da definida não serão exibidos.'
+              )}
+            </Text>
+            <CustomSlider
+              value={maxDistance}
+              maximumValue={20000}
+              step={500}
+              onValueChange={setMaxDistance}
+            />
+            <View
+              style={{
+                flexDirection: 'row',
+                alignItems: 'center',
+                marginTop: padding,
+                justifyContent: 'space-between',
+                marginBottom: 8,
+              }}
+            >
+              <Text style={{ ...texts.medium }}>{t('Distância Máxima até a Origem')}</Text>
+              <RoundedText>{formatDistance(maxDistanceToOrigin)}</RoundedText>
+            </View>
+            <Text style={{ ...texts.default, color: colors.darkGrey }}>
+              {t(
+                'Defina em Km a distância máxima da posição atual até a origem do pedido que essa frota poderá percorrer. Pedidos recebidos com origem acima da definida não serão exibidos.'
+              )}
+            </Text>
+            <CustomSlider
+              value={maxDistanceToOrigin}
+              maximumValue={20000}
+              step={500}
+              onValueChange={setMaxDistanceToOrigin}
+            />
+            <View
+              style={{
+                flexDirection: 'row',
+                alignItems: 'center',
+                marginTop: padding,
+                justifyContent: 'space-between',
+                marginBottom: 8,
+              }}
+            >
+              <Text style={{ ...texts.medium }}>{t('Porcentagem do Valor do Pedido')}</Text>
+              <RoundedText>{formatPct(feePctOverValue)}</RoundedText>
+            </View>
+            <Text style={{ ...texts.default, color: colors.darkGrey }}>
+              {t(
+                'Defina um percentual do valor do pedido em restaurantes que será adicionado ao pagamento dessa frota.'
+              )}
+            </Text>
+            <CustomSlider
+              value={feePctOverValue}
+              maximumValue={20000}
+              step={500}
+              onValueChange={setFeePcTOverValue}
+            />
+            <View
+              style={{
+                flexDirection: 'row',
+                alignItems: 'center',
+                marginTop: padding,
+                justifyContent: 'space-between',
+                marginBottom: 8,
+              }}
+            >
+              <Text style={{ ...texts.medium }}>{t('Valor Mínimo para Porcentagem')}</Text>
+              <RoundedText>{formatCurrency(valueThreshold)}</RoundedText>
+            </View>
+            <Text style={{ ...texts.default, color: colors.darkGrey }}>
+              {t(
+                'Defina um valor mínimo do pedido em restaurantes para que se comece a cobrar a porcentagem definida acima.'
+              )}
+            </Text>
+            <CustomSlider
+              value={valueThreshold}
+              maximumValue={20000}
+              step={500}
+              onValueChange={setValueThreshold}
+            />
           </View>
-          <View style={{ flexDirection: 'row', alignItems: 'center', flexWrap: 'wrap' }}>
-            <Text style={{ ...texts.medium }}>{t('Distância máxima até o ponto de coleta: ')}</Text>
-            <RoundedText>{formatDistance(maxDistanceToOrigin)}</RoundedText>
-          </View>
-          <CustomSlider
-            value={maxDistanceToOrigin}
-            maximumValue={20000}
-            step={500}
-            onValueChange={setMaxDistanceToOrigin}
-          />
-          <View style={{ flexDirection: 'row', alignItems: 'center', flexWrap: 'wrap' }}>
-            <Text style={{ ...texts.medium }}>{t('Distância máxima até a entrega: ')}</Text>
-            <RoundedText>{formatDistance(maxDistance)}</RoundedText>
-          </View>
-          <CustomSlider
-            value={maxDistance}
-            maximumValue={20000}
-            step={500}
-            onValueChange={setMaxDistance}
-          />
+        </PaddedView>
+        <View style={{ ...screens.default }}>
+          <PaddedView>
+            <Text style={{ ...texts.default, marginBottom: 4 }}>{t('Resumo da Frota')}</Text>
+            <Text style={{ ...texts.small, color: colors.darkGrey, marginBottom: 16 }}>
+              {t('Revise as informações antes de criar essa frota')}
+            </Text>
+            <View
+              style={{
+                flexDirection: 'row',
+                alignItems: 'center',
+                justifyContent: 'space-between',
+                marginBottom: 4,
+              }}
+            >
+              <Text style={{ ...texts.default }}>{t('Pagamento Mínimo')}</Text>
+              <Text style={{ ...texts.default }}>{formatCurrency(minimumFee)}</Text>
+            </View>
+            <View
+              style={{
+                flexDirection: 'row',
+                alignItems: 'center',
+                justifyContent: 'space-between',
+                marginBottom: 4,
+              }}
+            >
+              <Text style={{ ...texts.default }}>{t('Distância Mínima')}</Text>
+              <Text style={{ ...texts.default }}>{formatDistance(distanceThreshold)}</Text>
+            </View>
+            <View
+              style={{
+                flexDirection: 'row',
+                alignItems: 'center',
+                justifyContent: 'space-between',
+                marginBottom: 4,
+              }}
+            >
+              <Text style={{ ...texts.default, color: colors.darkGrey }}>
+                {t('Distância Máxima para Entrega')}
+              </Text>
+              <Text style={{ ...texts.default, color: colors.darkGrey }}>
+                {formatDistance(maxDistance)}
+              </Text>
+            </View>
+            <View
+              style={{
+                flexDirection: 'row',
+                alignItems: 'center',
+                justifyContent: 'space-between',
+                marginBottom: 4,
+              }}
+            >
+              <Text style={{ ...texts.default, color: colors.darkGrey }}>
+                {t('Distância Máxima até a Origem')}
+              </Text>
+              <Text style={{ ...texts.default, color: colors.darkGrey }}>
+                {formatDistance(maxDistanceToOrigin)}
+              </Text>
+            </View>
+            <View
+              style={{
+                flexDirection: 'row',
+                alignItems: 'center',
+                justifyContent: 'space-between',
+                marginBottom: 4,
+              }}
+            >
+              <Text style={{ ...texts.default, color: colors.darkGrey }}>
+                {t('Porcentagem do Valor do Pedido')}
+              </Text>
+              <Text style={{ ...texts.default, color: colors.darkGrey }}>
+                {formatPct(feePctOverValue)}
+              </Text>
+            </View>
+            <View
+              style={{
+                flexDirection: 'row',
+                alignItems: 'center',
+                justifyContent: 'space-between',
+                marginBottom: 4,
+              }}
+            >
+              <Text style={{ ...texts.default, color: colors.darkGrey }}>
+                {t('Valor Mínimo para Porcentagem')}
+              </Text>
+              <Text style={{ ...texts.default, color: colors.darkGrey }}>
+                {formatCurrency(valueThreshold)}
+              </Text>
+            </View>
+          </PaddedView>
         </View>
-        <DefaultButton
-          style={{ marginTop: padding * 2 }}
-          title={t('Confirmar')}
-          onPress={createFleetHandler}
-          disabled={!canSubmit || busy}
-          activityIndicator={busy}
-        />
-      </PaddedView>
+        <PaddedView>
+          {/* gains simulation should go here */}
+          <DefaultButton
+            style={{ marginTop: padding * 2 }}
+            title={t('Confirmar')}
+            onPress={createFleetHandler}
+            disabled={!canSubmit || busy}
+            activityIndicator={busy}
+          />
+        </PaddedView>
+      </View>
     </ScrollView>
   );
 }
