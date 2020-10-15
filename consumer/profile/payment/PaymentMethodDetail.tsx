98f082aa6363da9a2ee30e81718ee61a97d2fd7d
Daniel Lima <danielfbl@gmail.com>
2020-10-15 11:23:36 -0300

    merging

---
+++ consumer/profile/payment/PaymentMethodDetail.tsx
@@ -0,0 +1,12 @@
+import { RouteProp } from '@react-navigation/native';
+import { StackNavigationProp } from '@react-navigation/stack';
+import React from 'react';
+import { Text, View } from 'react-native';
+
+export default function () {
+  return (
+    <View>
+      <Text>DAdos do cartão</Text>
+    </View>
+  )
+}
