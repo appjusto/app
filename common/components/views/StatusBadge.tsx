c5709014a63dbebbf655e189fc578c4d1c705fd8
Pedro Andrade <pdandradeb@gmail.com>
2021-01-15 17:38:22 -0300

    Updating `StatusBadge`

--- common/components/views/StatusBadge.tsx
+++ common/components/views/StatusBadge.tsx
@@ -1,15 +1,16 @@
 import { OrderStatus } from 'appjusto-types';
 import React from 'react';
-
 import { t } from '../../../strings';
 import { colors } from '../../styles';
 import RoundedText from '../texts/RoundedText';
 
 const getStatusLabel = (status: OrderStatus) => {
   if (status === 'quote') return t('Cotação');
-  if (status === 'confirming') return t('Confirmando com restaurante');
-  if (status === 'matching') return t('Procurando entregadores');
-  if (status === 'dispatching') return t('Em andamento');
+  if (status === 'confirming') return t('Confirmando...');
+  if (status === 'confirmed') return t('Confirmado');
+  if (status === 'ready') return t('Aguardando entregador');
+  if (status === 'preparing') return t('Preparando');
+  if (status === 'dispatching') return t('Saiu para entrega');
   if (status === 'delivered') return t('Entregue');
   if (status === 'canceled') return t('Cancelado');
   return '';
@@ -21,7 +22,7 @@ type Props = {
 
 export default function ({ status }: Props) {
   let backgroundColor = colors.white;
-  if (status === 'confirming' || status === 'matching' || status === 'dispatching') backgroundColor = colors.yellow;
+  if (status === 'confirming' || status === 'dispatching') backgroundColor = colors.yellow;
   else if (status === 'canceled') backgroundColor = colors.lightRed;
   return <RoundedText backgroundColor={backgroundColor}>{getStatusLabel(status)}</RoundedText>;
 }
