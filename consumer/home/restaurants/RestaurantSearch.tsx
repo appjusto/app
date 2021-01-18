8a748d5e679dfd04df6ad247e051704ae188fc5c
Daniel Lima <danielfbl@gmail.com>
2021-01-18 16:23:54 -0300

    added logic to a basic name search in the RestaurantSearch screen

--- consumer/home/restaurants/RestaurantSearch.tsx
+++ consumer/home/restaurants/RestaurantSearch.tsx
@@ -1,11 +1,14 @@
 import { StackNavigationProp } from '@react-navigation/stack';
-import React, { useCallback, useRef, useState } from 'react';
+import React, { useState } from 'react';
 import { Image, View } from 'react-native';
-import { ScrollView, TextInput, TouchableOpacity } from 'react-native-gesture-handler';
+import { FlatList, TouchableOpacity } from 'react-native-gesture-handler';
 import * as icons from '../../../assets/icons';
+import PaddedView from '../../../common/components/containers/PaddedView';
 import DefaultInput from '../../../common/components/inputs/DefaultInput';
 import { padding, screens } from '../../../common/styles';
 import FilterButton from './components/FilterButton';
+import RestaurantListItem from './components/RestaurantListItem';
+import SingleHeader from './SingleHeader';
 import { RestaurantsNavigatorParamList } from './types';
 
 type ScreenNavigationProp = StackNavigationProp<RestaurantsNavigatorParamList>;
@@ -15,34 +18,65 @@ type Props = {
 };
 
 export default function ({ navigation }: Props) {
-  // context
-  // const api = useContext(ApiContext);
-  // const dispatch = useDispatch<AppDispatch>();
-
-  // refs
-  const searchInputRef = useRef<TextInput>();
-
   //state
   const [searchText, setSearchText] = useState<string>('');
+  const [searchData, setSearchData] = useState<RestaurantListItem[]>();
+  const [resultCount, setResultCount] = useState<number>(0);
+
+  type RestaurantListItem = {
+    onPress: () => void;
+    name: string;
+    cuisine: string;
+    deliveryRange: number;
+    id: string;
+  };
+
+  const data: RestaurantListItem[] = [
+    {
+      onPress: () => null,
+      name: 'Assis, o rei da Picanha',
+      cuisine: 'Churrasco',
+      deliveryRange: 4,
+      id: '0',
+    },
+    {
+      onPress: () => null,
+      name: 'Disk Pizza',
+      cuisine: 'Pizzaria',
+      deliveryRange: 5,
+      id: '1',
+    },
+    {
+      onPress: () => null,
+      name: 'Picanha da Aldeota',
+      cuisine: 'Churrasco',
+      deliveryRange: 6,
+      id: '2',
+    },
+  ];
 
   // handlers
-  // fires whenever use change the input text
-  const textChangeHandler = useCallback(
-    (text) => {
-      setSearchText(text); // update source text
-    },
-    [searchText]
-  );
+  const [dataSource, setDataSource] = useState<RestaurantListItem[]>(data);
+  const getSearchResults = (text: string) => {
+    if (text) {
+      const newData = dataSource.filter((item) => {
+        const name = item.name ? item.name.toLowerCase() : ''.toLowerCase();
+        const textData = text.toLowerCase();
+        return name.indexOf(textData) > -1;
+      });
+      setSearchData(newData);
+      setResultCount(newData.length);
+    }
+  };
 
   //UI
   return (
-    <ScrollView style={{ ...screens.default }}>
-      <View style={{ paddingHorizontal: 12, marginTop: padding }}>
+    <View style={{ ...screens.default }}>
+      <PaddedView>
         <DefaultInput
-          ref={searchInputRef}
           defaultValue={searchText}
           value={searchText}
-          onChangeText={textChangeHandler}
+          onChangeText={setSearchText}
           autoCorrect={false}
           style={{ paddingVertical: padding, paddingLeft: 12 }}
         />
@@ -50,18 +84,43 @@ export default function ({ navigation }: Props) {
           style={{
             position: 'absolute',
             right: 24,
-            bottom: padding,
+            bottom: padding * 2,
             flex: 1,
           }}
         >
-          <TouchableOpacity onPress={() => null}>
+          <TouchableOpacity
+            onPress={() => {
+              getSearchResults(searchText);
+              // console.log(searchData);
+            }}
+          >
             <Image source={icons.search} />
           </TouchableOpacity>
         </View>
-      </View>
-      <View style={{ marginHorizontal: 12, marginTop: padding }}>
+      </PaddedView>
+      <View style={{ marginBottom: padding }}>
         <FilterButton onPress={() => navigation.navigate('OrderBy')} />
       </View>
-    </ScrollView>
+
+      {searchData && (
+        <FlatList
+          data={searchData}
+          ListHeaderComponent={
+            searchData ? <SingleHeader title={`${resultCount} resultados encontrados`} /> : null
+          }
+          keyExtractor={(item) => item.id}
+          renderItem={({ item }) => (
+            <View>
+              <RestaurantListItem
+                onPress={item.onPress}
+                cuisine={item.cuisine}
+                name={item.name}
+                deliveryRange={item.deliveryRange}
+              />
+            </View>
+          )}
+        />
+      )}
+    </View>
   );
 }
