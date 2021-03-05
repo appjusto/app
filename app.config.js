c6f635b6af71c8d049fe8f40def42c265486b5fd
Pedro Andrade <pdandradeb@gmail.com>
2021-03-05 15:43:06 -0300

    Bumping version to 0.7.0

--- app.config.js
+++ app.config.js
@@ -21,8 +21,8 @@ const {
 } = process.env;
 
 export default () => {
-  const version = '0.6.0';
-  const versionCode = 11;
+  const version = '0.7.0';
+  const versionCode = 12;
   const flavor = process.env.FLAVOR;
   const appId = `com.appjusto.${flavor}`;
   const icon = (platform) => `./assets/icon-${flavor}-${platform}.png`;
