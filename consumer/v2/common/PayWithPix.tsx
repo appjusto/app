6a2d598cd3ac4b2532035ac69d892462251b6f18
Daniel Lima <danielfbl@gmail.com>
2021-03-16 17:08:55 -0300

    finished the basic layout for PayWithPix

--- consumer/v2/common/PayWithPix.tsx
+++ consumer/v2/common/PayWithPix.tsx
@@ -1,17 +1,61 @@
 import React from 'react';
 import { Image, Text, View } from 'react-native';
 import { pix } from '../../../assets/icons';
+import DefaultButton from '../../../common/components/buttons/DefaultButton';
 import PaddedView from '../../../common/components/containers/PaddedView';
-import { padding, screens, texts } from '../../../common/styles';
+import DefaultInput from '../../../common/components/inputs/DefaultInput';
+import Pill from '../../../common/components/views/Pill';
+import { colors, padding, screens, texts } from '../../../common/styles';
 import { t } from '../../../strings';
 
 export const PayWithPix = () => {
   return (
     <View style={{ ...screens.config }}>
-      <PaddedView>
+      <PaddedView style={{ flex: 1 }}>
         <Image source={pix} />
         <Text style={{ ...texts.lg, marginTop: padding }}>{t('Informe sua chave')}</Text>
+        <Text style={{ ...texts.sm, marginVertical: padding, color: colors.grey700 }}>
+          {t(
+            'É importante informar a sua chave para enviarmos o estorno do valor caso ocorra algum problema no pedido.'
+          )}
+        </Text>
+        <DefaultInput title={t('Chave Pix')} />
+        {/* add a radio/check with 'adicionar cpf' here */}
       </PaddedView>
+      <View style={{ flex: 1 }} />
+      <View
+        style={{
+          backgroundColor: colors.white,
+          paddingTop: padding,
+          paddingRight: padding,
+          // flex: 1,
+          paddingBottom: 32,
+        }}
+      >
+        <View
+          style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center' }}
+        >
+          <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+            <Pill />
+            <Text style={{ ...texts.md, marginLeft: 12 }}>{t('Valor total a pagar')}</Text>
+          </View>
+          <Text style={{ ...texts.xl }}>{t('R$ 10,00')}</Text>
+        </View>
+        <View style={{ marginTop: padding, marginHorizontal: padding }}>
+          <Text
+            style={{
+              ...texts.xs,
+              color: colors.grey700,
+              marginBottom: padding,
+            }}
+          >
+            {t(
+              'Você poderá deixar uma Caixinha de gorjeta para o entregador quando o seu pedido for entregue.'
+            )}
+          </Text>
+          <DefaultButton title={t('Gerar código de pagamento Pix')} />
+        </View>
+      </View>
     </View>
   );
 };
