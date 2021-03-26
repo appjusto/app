452f66ca943b9ba7f44a173a06601f59b0a2204a
Daniel Lima <danielfbl@gmail.com>
2021-03-26 13:30:17 -0300

    restoring the WelcomeScreen

--- common/screens/unlogged/WelcomeScreen.tsx
+++ common/screens/unlogged/WelcomeScreen.tsx
@@ -16,6 +16,7 @@ import { t } from '../../../strings';
 import { ApiContext, AppDispatch } from '../../app/context';
 import CheckField from '../../components/buttons/CheckField';
 import DefaultButton from '../../components/buttons/DefaultButton';
+import AvoidingView from '../../components/containers/AvoidingView';
 import DefaultInput from '../../components/inputs/DefaultInput';
 import { getFlavor } from '../../store/config/selectors';
 import { showToast } from '../../store/ui/actions';
@@ -69,80 +70,83 @@ export default function ({ navigation, route }: Props) {
 
   // UI
   return (
-    <View style={{ ...screens.default, justifyContent: 'flex-end' }}>
-      <View style={{ paddingHorizontal: padding }}>
-        <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
-          <View>
-            <View style={{ height: 200, marginTop: 30, width: 275 }}>
-              <Image source={flavor === 'consumer' ? illustration : motoIntro} />
-            </View>
+    <View style={[screens.default]}>
+      <AvoidingView>
+        <View style={{ flex: 1, justifyContent: 'flex-end' }}>
+          <View style={{ paddingHorizontal: padding }}>
+            <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+              <View>
+                <View style={{ height: 200, marginTop: 30, width: 275 }}>
+                  <Image source={flavor === 'consumer' ? illustration : motoIntro} />
+                </View>
 
-            <View style={{ height: 64, marginTop: 16, width: 152 }}>
-              <Image style={{ height: '100%', width: '100%' }} source={logoWhite} />
-            </View>
+                <View style={{ height: 64, marginTop: 16, width: 152 }}>
+                  <Image style={{ height: '100%', width: '100%' }} source={logoWhite} />
+                </View>
 
-            <View style={{ height: 58, marginTop: 16 }}>
-              <Text style={[texts.x2l]}>{welcomeMessage}</Text>
-            </View>
+                <View style={{ height: 58, marginTop: 16 }}>
+                  <Text style={[texts.x2l]}>{welcomeMessage}</Text>
+                </View>
 
-            <View style={{ width: '85%', height: 58, marginTop: 16 }}>
-              <Text style={[texts.sm, { color: colors.grey700, lineHeight: 21 }]}>
-                {t('Digite seu e-mail para entrar ou criar sua conta.')}
-              </Text>
-            </View>
-          </View>
-        </TouchableWithoutFeedback>
+                <View style={{ width: '85%', height: 58, marginTop: 16 }}>
+                  <Text style={[texts.sm, { color: colors.grey700, lineHeight: 21 }]}>
+                    {t('Digite seu e-mail para entrar ou criar sua conta.')}
+                  </Text>
+                </View>
+              </View>
+            </TouchableWithoutFeedback>
 
-        <DefaultInput
-          value={email}
-          title={t('Acesse sua conta')}
-          placeholder={t('Digite seu e-mail')}
-          onChangeText={setEmail}
-          keyboardType="email-address"
-          blurOnSubmit
-          autoCapitalize="none"
-        />
-
-        <View
-          style={{
-            flexDirection: 'row',
-            alignItems: 'center',
-            justifyContent: 'space-between',
-            marginTop: height > 700 ? 32 : 16,
-          }}
-        >
-          <View>
-            <CheckField
-              checked={acceptedTerms}
-              onPress={() => setAcceptTerms(!acceptedTerms)}
-              text={t('Aceito os termos de uso do app')}
+            <DefaultInput
+              value={email}
+              title={t('Acesse sua conta')}
+              placeholder={t('Digite seu e-mail')}
+              onChangeText={setEmail}
+              keyboardType="email-address"
+              blurOnSubmit
+              autoCapitalize="none"
             />
-          </View>
-          <View>
-            <TouchableOpacity
-              onPress={() => {
-                navigation.navigate('Terms');
+            <View
+              style={{
+                flexDirection: 'row',
+                alignItems: 'center',
+                justifyContent: 'space-between',
+                marginTop: height > 700 ? 32 : 16,
               }}
             >
-              <Text style={[texts.xs, { color: colors.green600, lineHeight: 18 }]}>
-                {t('Ler os termos')}
-              </Text>
-            </TouchableOpacity>
+              <View>
+                <CheckField
+                  checked={acceptedTerms}
+                  onPress={() => setAcceptTerms(!acceptedTerms)}
+                  text={t('Aceito os termos de uso do app')}
+                />
+              </View>
+              <View>
+                <TouchableOpacity
+                  onPress={() => {
+                    navigation.navigate('Terms');
+                  }}
+                >
+                  <Text style={[texts.xs, { color: colors.green600, lineHeight: 18 }]}>
+                    {t('Ler os termos')}
+                  </Text>
+                </TouchableOpacity>
+              </View>
+            </View>
+            {/* dummy view to accomadate keyboard better */}
+            <View style={{ height: 20 }} />
+          </View>
+
+          <View style={{ flex: 1 }} />
+          <View style={{ paddingHorizontal: 16, marginBottom: height > 700 ? 32 : 16 }}>
+            <DefaultButton
+              disabled={validateEmail(email).status !== 'ok' || !acceptedTerms || busy}
+              title={t('Entrar')}
+              onPress={signInHandler}
+              activityIndicator={busy}
+            />
           </View>
         </View>
-        {/* dummy view to accomadate keyboard better */}
-        <View style={{ height: 20 }} />
-      </View>
-
-      <View style={{ flex: 1 }} />
-      <View style={{ paddingHorizontal: 16, marginBottom: height > 700 ? 32 : 16 }}>
-        <DefaultButton
-          disabled={validateEmail(email).status !== 'ok' || !acceptedTerms || busy}
-          title={t('Entrar')}
-          onPress={signInHandler}
-          activityIndicator={busy}
-        />
-      </View>
+      </AvoidingView>
     </View>
   );
 }
