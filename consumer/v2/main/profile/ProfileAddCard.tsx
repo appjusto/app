9cc5c4a27bc04153a0379f50ed537502ae8d8031
Daniel Lima <danielfbl@gmail.com>
2021-04-09 15:48:08 -0300

    layout adjustment

--- consumer/v2/main/profile/ProfileAddCard.tsx
+++ consumer/v2/main/profile/ProfileAddCard.tsx
@@ -5,6 +5,7 @@ import { StackNavigationProp } from '@react-navigation/stack';
 import { isEmpty, toNumber, trim } from 'lodash';
 import React from 'react';
 import { Text, TextInput, View } from 'react-native';
+import { ScrollView } from 'react-native-gesture-handler';
 import { useDispatch } from 'react-redux';
 import { ApiContext, AppDispatch } from '../../../../common/app/context';
 import DefaultButton from '../../../../common/components/buttons/DefaultButton';
@@ -101,157 +102,160 @@ export default function ({ navigation, route }: Props) {
   const cpfRef = React.useRef<TextInput>(null);
   // UI
   return (
-    <View style={screens.config}>
-      <View style={{ flex: 1, padding }}>
-        <PatternInput
-          title={t('Número do cartão')}
-          value={number}
-          placeholder={t('0000 0000 0000 0000')}
-          mask={cardMask}
-          parser={numbersOnlyParser}
-          formatter={cardFormatter}
-          keyboardType="number-pad"
-          textContentType="creditCardNumber"
-          autoCompleteType="cc-number"
-          returnKeyType="next"
-          blurOnSubmit={false}
-          onChangeText={(text) => {
-            if (!isNaN(toNumber(text))) setNumber(text);
-          }}
-          onSubmitEditing={() => expirationMonthRef.current?.focus()}
-        />
-        <View style={{ flexDirection: 'row', marginTop: padding }}>
-          <DefaultInput
-            ref={expirationMonthRef}
-            style={{ flex: 2, marginRight: padding }}
-            title={t('Mês de validade')}
-            value={month}
-            placeholder={t('00')}
-            maxLength={2}
+    <ScrollView contentContainerStyle={screens.config}>
+      <View style={{ flex: 1 }}>
+        <View style={{ flex: 1, padding }}>
+          <PatternInput
+            title={t('Número do cartão')}
+            value={number}
+            placeholder={t('0000 0000 0000 0000')}
+            mask={cardMask}
+            parser={numbersOnlyParser}
+            formatter={cardFormatter}
             keyboardType="number-pad"
+            textContentType="creditCardNumber"
+            autoCompleteType="cc-number"
             returnKeyType="next"
-            autoCompleteType="cc-exp-month"
             blurOnSubmit={false}
             onChangeText={(text) => {
-              if (!isNaN(toNumber(text))) setMonth(text);
+              if (!isNaN(toNumber(text))) setNumber(text);
             }}
-            onSubmitEditing={() => expirationYearRef.current?.focus()}
+            onSubmitEditing={() => expirationMonthRef.current?.focus()}
           />
-          <DefaultInput
-            ref={expirationYearRef}
-            style={{ flex: 2, marginRight: padding }}
-            title={t('Ano de validade')}
-            value={year}
-            placeholder={t('0000')}
-            maxLength={4}
+          <View style={{ flexDirection: 'row', marginTop: padding }}>
+            <DefaultInput
+              ref={expirationMonthRef}
+              style={{ flex: 2, marginRight: padding }}
+              title={t('Mês de validade')}
+              value={month}
+              placeholder={t('00')}
+              maxLength={2}
+              keyboardType="number-pad"
+              returnKeyType="next"
+              autoCompleteType="cc-exp-month"
+              blurOnSubmit={false}
+              onChangeText={(text) => {
+                if (!isNaN(toNumber(text))) setMonth(text);
+              }}
+              onSubmitEditing={() => expirationYearRef.current?.focus()}
+            />
+            <DefaultInput
+              ref={expirationYearRef}
+              style={{ flex: 2, marginRight: padding }}
+              title={t('Ano de validade')}
+              value={year}
+              placeholder={t('0000')}
+              maxLength={4}
+              keyboardType="number-pad"
+              returnKeyType="next"
+              autoCompleteType="cc-exp-year"
+              blurOnSubmit={false}
+              onChangeText={(text) => {
+                if (!isNaN(toNumber(text))) setYear(text);
+              }}
+              onSubmitEditing={() => cvvRef.current?.focus()}
+            />
+            <DefaultInput
+              ref={cvvRef}
+              style={{ flex: 1 }}
+              title={t('CVV')}
+              value={cvv}
+              placeholder={t('000')}
+              maxLength={3}
+              keyboardType="number-pad"
+              returnKeyType="next"
+              autoCompleteType="cc-csc"
+              blurOnSubmit={false}
+              onChangeText={(text) => {
+                if (!isNaN(toNumber(text))) setCVV(text);
+              }}
+              onSubmitEditing={() => nameRef.current?.focus()}
+            />
+          </View>
+          <View style={{ flexDirection: 'row', marginTop: padding }}>
+            <DefaultInput
+              ref={nameRef}
+              style={{ flex: 1, marginRight: padding }}
+              title={t('Nome ')}
+              value={name}
+              placeholder={t('Conforme cartão')}
+              keyboardType="name-phone-pad"
+              returnKeyType="next"
+              textContentType="givenName"
+              autoCompleteType="name"
+              autoCapitalize="characters"
+              blurOnSubmit={false}
+              onChangeText={setName}
+              onSubmitEditing={() => surnameRef.current?.focus()}
+            />
+            <DefaultInput
+              ref={surnameRef}
+              style={{ flex: 1 }}
+              title={t('Sobrenome')}
+              value={surname}
+              placeholder={t('Conforme cartão')}
+              keyboardType="name-phone-pad"
+              textContentType="familyName"
+              autoCompleteType="name"
+              autoCapitalize="characters"
+              returnKeyType="next"
+              blurOnSubmit={false}
+              onChangeText={setSurname}
+              onSubmitEditing={() => cpfRef.current?.focus()}
+            />
+          </View>
+          <PatternInput
+            ref={cpfRef}
+            style={{ marginTop: padding }}
+            title={t('CPF do titular')}
+            value={cpf}
+            placeholder={t('Seu CPF, apenas números')}
+            mask={cpfMask}
+            parser={numbersOnlyParser}
+            formatter={cpfFormatter}
             keyboardType="number-pad"
-            returnKeyType="next"
-            autoCompleteType="cc-exp-year"
-            blurOnSubmit={false}
+            returnKeyType="done"
+            blurOnSubmit
+            onFocus={() => setFocusedField('cpf')}
+            onBlur={() => setFocusedField(undefined)}
             onChangeText={(text) => {
-              if (!isNaN(toNumber(text))) setYear(text);
+              if (!isNaN(toNumber(text))) setCPF(text);
             }}
-            onSubmitEditing={() => cvvRef.current?.focus()}
           />
-          <DefaultInput
-            ref={cvvRef}
-            style={{ flex: 1 }}
-            title={t('CVV')}
-            value={cvv}
-            placeholder={t('000')}
-            maxLength={3}
-            keyboardType="number-pad"
-            returnKeyType="next"
-            autoCompleteType="cc-csc"
-            blurOnSubmit={false}
-            onChangeText={(text) => {
-              if (!isNaN(toNumber(text))) setCVV(text);
-            }}
-            onSubmitEditing={() => nameRef.current?.focus()}
-          />
-        </View>
-        <View style={{ flexDirection: 'row', marginTop: padding }}>
-          <DefaultInput
-            ref={nameRef}
-            style={{ flex: 1, marginRight: padding }}
-            title={t('Nome ')}
-            value={name}
-            placeholder={t('Conforme cartão')}
-            keyboardType="name-phone-pad"
-            returnKeyType="next"
-            textContentType="givenName"
-            autoCompleteType="name"
-            autoCapitalize="characters"
-            blurOnSubmit={false}
-            onChangeText={setName}
-            onSubmitEditing={() => surnameRef.current?.focus()}
-          />
-          <DefaultInput
-            ref={surnameRef}
-            style={{ flex: 1 }}
-            title={t('Sobrenome')}
-            value={surname}
-            placeholder={t('Conforme cartão')}
-            keyboardType="name-phone-pad"
-            textContentType="familyName"
-            autoCompleteType="name"
-            autoCapitalize="characters"
-            returnKeyType="next"
-            blurOnSubmit={false}
-            onChangeText={setSurname}
-            onSubmitEditing={() => cpfRef.current?.focus()}
-          />
-        </View>
-        <PatternInput
-          ref={cpfRef}
-          style={{ marginTop: padding }}
-          title={t('CPF do titular')}
-          value={cpf}
-          placeholder={t('Seu CPF, apenas números')}
-          mask={cpfMask}
-          parser={numbersOnlyParser}
-          formatter={cpfFormatter}
-          keyboardType="number-pad"
-          returnKeyType="done"
-          blurOnSubmit
-          onFocus={() => setFocusedField('cpf')}
-          onBlur={() => setFocusedField(undefined)}
-          onChangeText={(text) => {
-            if (!isNaN(toNumber(text))) setCPF(text);
-          }}
-        />
-        {cpf.length > 0 && !cpfutils.isValid(cpf) && focusedField !== 'cpf' && (
-          <Text style={{ ...texts.sm, ...texts.bold, color: colors.grey700, marginTop: padding }}>
-            {t('O CPF digitado não é válido.')}
-          </Text>
-        )}
-      </View>
-      <View style={{ flex: 1 }} />
-      <View style={{ backgroundColor: colors.white, flex: 1, padding }}>
-        <View style={{ flexDirection: 'row', alignItems: 'center', marginBottom: padding }}>
-          <Feather name="info" size={14} />
-          <Text style={{ ...texts.md, marginLeft: halfPadding }}>
-            {t('Informações sobre a cobrança')}
-          </Text>
-        </View>
-        <Text style={{ ...texts.xs }}>
-          {t(
-            'Para sermos mais justos, o AppJusto cobra o valor total do pedido divido em duas cobranças no cartão. Por exemplo, se o seu pedido é no valor de R$ 10, vamos gerar duas cobranças que somadas custarão R$ 10.'
+          {cpf.length > 0 && !cpfutils.isValid(cpf) && focusedField !== 'cpf' && (
+            <Text style={{ ...texts.sm, ...texts.bold, color: colors.grey700, marginTop: padding }}>
+              {t('O CPF digitado não é válido.')}
+            </Text>
           )}
-        </Text>
-        <Text style={{ ...texts.xs, marginTop: padding }}>
-          {t(
-            'Cada uma dessas cobranças vai pra uma conta específica, facilitando a divisão entre restaurantes e entregadores.'
-          )}
-        </Text>
-        <DefaultButton
-          style={{ marginTop: padding }}
-          title={t('Salvar')}
-          onPress={saveCardHandler}
-          disabled={!canSubmit || isLoading}
-          activityIndicator={isLoading}
-        />
+        </View>
+
+        <View style={{ flex: 1 }} />
+        <View style={{ backgroundColor: colors.white, flex: 1, padding }}>
+          <View style={{ flexDirection: 'row', alignItems: 'center', marginBottom: padding }}>
+            <Feather name="info" size={14} />
+            <Text style={{ ...texts.md, marginLeft: halfPadding }}>
+              {t('Informações sobre a cobrança')}
+            </Text>
+          </View>
+          <Text style={{ ...texts.xs }}>
+            {t(
+              'Para sermos mais justos, o AppJusto cobra o valor total do pedido divido em duas cobranças no cartão. Por exemplo, se o seu pedido é no valor de R$ 10, vamos gerar duas cobranças que somadas custarão R$ 10.'
+            )}
+          </Text>
+          <Text style={{ ...texts.xs, marginTop: padding }}>
+            {t(
+              'Cada uma dessas cobranças vai pra uma conta específica, facilitando a divisão entre restaurantes e entregadores.'
+            )}
+          </Text>
+          <DefaultButton
+            style={{ marginTop: padding }}
+            title={t('Salvar')}
+            onPress={saveCardHandler}
+            disabled={!canSubmit || isLoading}
+            activityIndicator={isLoading}
+          />
+        </View>
       </View>
-    </View>
+    </ScrollView>
   );
 }
