cf30788cabfd073c6d69ebd0a220203e78f353c1
Daniel Lima <danielfbl@gmail.com>
2021-04-22 16:22:50 -0300

    added OnboardingSteps and some dynamic content

--- common/screens/unlogged/Onboarding.tsx
+++ common/screens/unlogged/Onboarding.tsx
@@ -1,20 +1,67 @@
 import React from 'react';
 import { ScrollView, Text, View } from 'react-native';
+import { useSelector } from 'react-redux';
 import { t } from '../../../strings';
 import DefaultButton from '../../components/buttons/DefaultButton';
-import { IconShare } from '../../icons/icon-share';
+import { IconBeta } from '../../icons/icon-beta';
+import { IconHangLoose } from '../../icons/icon-hang-loose';
+import { getFlavor } from '../../store/config/selectors';
 import { borders, halfPadding, padding, screens, texts } from '../../styles';
 
+export enum OnboardingSteps {
+  Welcome = 0,
+  WeAreBeta,
+  ForDelivery,
+  YouCanChoose,
+  Share,
+}
+
 export const Onboarding = () => {
+  // redux store
+  const flavor = useSelector(getFlavor);
+  // state
+  const [step, setStep] = React.useState(OnboardingSteps.Welcome);
   // estrututura:
   //icon: IconHangLoose, IconBeta, IconAppDelivery, IconHeartBox, IconShareBig
   //header
+  let headerTitle;
+  let topDescription;
+  let bottomDescription;
+  let icon;
+  let buttonTitle;
+  if (flavor === 'courier' && step === OnboardingSteps.Welcome) {
+    headerTitle = t('Olá,\n que legal ver você aqui!');
+    topDescription = t(
+      'Mais que um App de entregas, agora você faz parte de um movimento por relações mais justas e pelo fim da precarização do trabalho.'
+    );
+    bottomDescription = t(
+      'Estamos apenas começando. Com o comprometimento de todos, vamos dar certo. Ajude divulgando esse movimento!'
+    );
+    icon = <IconHangLoose />;
+    // stepDisplay
+    buttonTitle = t('Avançar');
+  }
+  if (flavor === 'courier' && step === OnboardingSteps.WeAreBeta) {
+    headerTitle = t('O AppJusto ainda está em fase de testes');
+    topDescription = t(
+      'Por enquanto, estamos em fase de validação e testes da plataforma. Queremos construir o melhor serviço pra você.'
+    );
+    bottomDescription = t(
+      'Fique à vontade para nos avisar pelo chat se perceber algum problema e também para contribuir com sugestões. Vamos juntos'
+    );
+    icon = <IconBeta />;
+    // stepDisplay
+    buttonTitle = t('Iniciar cadastro');
+  }
+
   //topDescription
   //bottomDescription
   // stepsDisplayer
   // buttonTitle
   const steps = [];
-  for (let i = 1; i <= 2; i += 1) {
+  const totalSteps = flavor === 'courier' ? 2 : 5;
+
+  for (let i = 1; i <= totalSteps; i += 1) {
     steps.push(
       <View
         style={{
@@ -29,23 +76,14 @@ export const Onboarding = () => {
       />
     );
   }
+
   return (
     <ScrollView style={[screens.default, { paddingHorizontal: padding }]}>
       <View style={{ justifyContent: 'center', alignItems: 'center', marginTop: 48 }}>
-        <IconShare />
-        <Text style={{ ...texts.x2l, marginTop: 32, textAlign: 'center' }}>
-          {t('Olá,\n boas vindas ao AppJusto!')}
-        </Text>
-        <Text style={{ ...texts.md, marginTop: 32, textAlign: 'center' }}>
-          {t(
-            'Mais que um app de delivery, somos um movimento por relações mais justas para clientes, restaurantes e entregadores.'
-          )}
-        </Text>
-        <Text style={{ ...texts.md, marginTop: 32, textAlign: 'center' }}>
-          {t(
-            'O AppJusto veio para combater a precarização do trabalho e, com a ajuda de todos, vamos fazer dar certo!'
-          )}
-        </Text>
+        {icon}
+        <Text style={{ ...texts.x2l, marginTop: 32, textAlign: 'center' }}>{headerTitle}</Text>
+        <Text style={{ ...texts.md, marginTop: 32, textAlign: 'center' }}>{topDescription}</Text>
+        <Text style={{ ...texts.md, marginTop: 32, textAlign: 'center' }}>{bottomDescription}</Text>
       </View>
       <View
         style={{
@@ -58,9 +96,9 @@ export const Onboarding = () => {
         {steps}
       </View>
       <DefaultButton
-        title={t('Avançar')}
+        title={buttonTitle ?? ''}
         style={{ marginTop: 32, marginBottom: padding }}
-        onPress={() => null}
+        onPress={() => setStep(step + 1)}
       />
     </ScrollView>
   );
