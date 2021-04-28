490eeb9c89a68d0811cba4ebe80f35d0a0f6205d
Pedro Andrade <pdandradeb@gmail.com>
2021-04-28 18:39:33 -0300

    Finishing AboutApp screen; Installing expo-device dependency;

--- common/screens/profile/AboutApp.tsx
+++ common/screens/profile/AboutApp.tsx
@@ -1,6 +1,8 @@
 import { StackNavigationProp } from '@react-navigation/stack';
+import Constants from 'expo-constants';
+import * as Device from 'expo-device';
 import React from 'react';
-import { View } from 'react-native';
+import { Linking, Platform, View } from 'react-native';
 import { ScrollView } from 'react-native-gesture-handler';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { ProfileParamList } from '../../../consumer/v2/main/profile/types';
@@ -8,8 +10,9 @@ import { CourierProfileParamList } from '../../../courier/approved/main/profile/
 import { t } from '../../../strings';
 import PaddedView from '../../components/containers/PaddedView';
 import ConfigItem from '../../components/views/ConfigItem';
+import { IconVersion } from '../../icons/icon-version';
 import { screens } from '../../styles';
-import VersionCard from '../home/cards/VersionCard';
+import HomeCard from '../home/cards/HomeCard';
 
 export type AboutAppParamList = {
   AboutApp: undefined;
@@ -25,32 +28,37 @@ type Props = {
 };
 
 export const AboutApp = ({ navigation }: Props) => {
+  const appVersion = `${t('Versão:')} ${Constants.nativeAppVersion} / ${
+    Constants.nativeBuildVersion
+  }`;
+  const brand = Device.brand ?? Device.manufacturer ?? '';
+  const model = Device.modelName ?? Device.modelId ?? Device.productName ?? '';
+  const os = `${brand} ${model} ${Device.osVersion} ${
+    Platform.OS === 'android' ? Device.platformApiLevel : ''
+  }`;
   return (
     <ScrollView style={{ ...screens.config }} contentContainerStyle={{ flexGrow: 1 }}>
       <View style={{ flex: 1 }}>
         <ConfigItem
           title={t('Central de ajuda')}
           subtitle={t('Tire suas dúvidas')}
-          onPress={() => null}
+          onPress={() => Linking.openURL('https://appjusto.freshdesk.com/')}
         />
         <ConfigItem
           title={t('Site oficial')}
           subtitle={t('Acesse nosso site')}
-          onPress={() => null}
+          onPress={() => Linking.openURL('https://appjusto.com.br/')}
         />
         <ConfigItem
           title={t('Código aberto')}
           subtitle={t('Acesse o repositório com o código no GitHub')}
-          onPress={() => null}
+          onPress={() => Linking.openURL('https://github.com/appjusto')}
         />
       </View>
       <View style={{ flex: 1 }} />
       <SafeAreaView>
         <PaddedView>
-          <VersionCard
-            title={t('Versão do aplicativo: {0000}')}
-            subtitle={t('Atualizado em 00/00/0000')}
-          />
+          <HomeCard icon={<IconVersion />} title={appVersion} subtitle={os} />
         </PaddedView>
       </SafeAreaView>
     </ScrollView>
