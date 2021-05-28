46211f9b16fde2a03ed8fbc179102b5af232d694
Pedro Andrade <pdandradeb@gmail.com>
2021-05-28 17:50:16 -0300

    WIP on notification handling

--- common/hooks/useNotificationToken.ts
+++ common/hooks/useNotificationToken.ts
@@ -2,53 +2,7 @@ import Constants from 'expo-constants';
 import * as Notifications from 'expo-notifications';
 import * as Permissions from 'expo-permissions';
 import { useEffect, useState } from 'react';
-import { Platform } from 'react-native';
 import * as Sentry from 'sentry-expo';
-import { t } from '../../strings';
-import { colors } from '../styles';
-
-Notifications.setNotificationHandler({
-  handleNotification: async () => ({
-    shouldShowAlert: true,
-    shouldPlaySound: true,
-    shouldSetBadge: false,
-  }),
-});
-
-if (Platform.OS === 'android') {
-  (async () => {
-    // await Notifications.deleteNotificationChannelAsync('order-request');
-    // const notificationChannels = await Notifications.getNotificationChannelsAsync();
-    // console.log(notificationChannels);
-    Notifications.setNotificationChannelAsync('order-request', {
-      name: t('Novas corridas'),
-      importance: Notifications.AndroidImportance.HIGH,
-      enableVibrate: true,
-      vibrationPattern: [0, 250, 250, 250],
-      lockscreenVisibility: Notifications.AndroidNotificationVisibility.PUBLIC,
-      bypassDnd: true,
-      lightColor: colors.green100,
-    });
-    Notifications.setNotificationChannelAsync('order-update', {
-      name: t('Atualizações do pedido'),
-      importance: Notifications.AndroidImportance.HIGH,
-      enableVibrate: true,
-      vibrationPattern: [0, 250, 250, 250],
-      lockscreenVisibility: Notifications.AndroidNotificationVisibility.PUBLIC,
-      bypassDnd: true,
-      lightColor: colors.green100,
-    });
-    Notifications.setNotificationChannelAsync('order-chat', {
-      name: t('Chat durante entrega'),
-      importance: Notifications.AndroidImportance.HIGH,
-      enableVibrate: true,
-      vibrationPattern: [0, 250, 250, 250],
-      lockscreenVisibility: Notifications.AndroidNotificationVisibility.PUBLIC,
-      bypassDnd: true,
-      lightColor: colors.green100,
-    });
-  })();
-}
 
 type ErrorType = 'permission-denied' | 'not-a-device';
 type Returntype = [string | null, boolean, boolean, ErrorType | null];
