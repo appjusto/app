3ec0e254ffaaf338b103b899208ad260fc1e36ed
Daniel Lima <danielfbl@gmail.com>
2021-06-01 16:03:35 -0300

    added a comment

--- courier/approved/ongoing/CourierDropsOrder.tsx
+++ courier/approved/ongoing/CourierDropsOrder.tsx
@@ -34,6 +34,7 @@ export const CourierDropsOrder = ({ navigation, route }: Props) => {
   const dispatch = useDispatch<AppDispatch>();
   // state
   const issues = useIssues('courier-refuse');
+  // const issues = useIssues('courier-drops-delivery');
   const courier = useSelector(getCourier)!;
   // screen state
   const [comment, setComment] = React.useState('');
