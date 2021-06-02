2a49432c9c2ef5b35a4a464eb912a99d3776e65d
Daniel Lima <danielfbl@gmail.com>
2021-06-02 13:47:32 -0300

    committing new screen file before merging another branch into this one

---
+++ consumer/v2/ongoing/OngoingOrderCancellation.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+export const OngoingOrderCancellation = () => {
+  return (
+    <View>
+      <Text>NOVA TELA</Text>
+    </View>
+  );
+};
