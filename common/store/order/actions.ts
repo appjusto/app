a63f068a8d041798e2d6df5e5c7d3ea860e22b0c
Pedro Andrade <pdandradeb@gmail.com>
2021-06-04 16:58:48 -0300

    Removing unused actions

--- common/store/order/actions.ts
+++ common/store/order/actions.ts
@@ -1,4 +1,4 @@
-import { Fare, OrderIssue } from '@appjusto/types';
+import { OrderIssue } from '@appjusto/types';
 import { AppDispatch } from '../../app/context';
 import Api from '../api/api';
 import { awaitWithFeedback } from '../ui/actions';
@@ -6,15 +6,6 @@ import { awaitWithFeedback } from '../ui/actions';
 export const ORDERS_UPDATED = 'ORDERS_UPDATED';
 
 // consumers
-
-export const getOrderQuotes = (api: Api) => (orderId: string) => async (dispatch: AppDispatch) => {
-  return dispatch(awaitWithFeedback<Fare[]>(api.order().getOrderQuotes(orderId)));
-};
-
-export const tipCourier =
-  (api: Api) => (orderId: string, tip: number) => async (dispatch: AppDispatch) => {
-    return dispatch(awaitWithFeedback(api.order().tipCourier(orderId, tip)));
-  };
 
 export const cancelOrder =
   (api: Api) => (orderId: string, cancellation?: OrderIssue) => async (dispatch: AppDispatch) => {
