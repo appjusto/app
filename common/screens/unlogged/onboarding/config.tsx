11b49b190d4ca91f07edd02285e2f9eda2a62316
Daniel Lima <danielfbl@gmail.com>
2021-06-08 15:48:38 -0300

    new icon and config for consumer onboarding

--- common/screens/unlogged/onboarding/config.tsx
+++ common/screens/unlogged/onboarding/config.tsx
@@ -1,10 +1,8 @@
 import React from 'react';
 import { t } from '../../../../strings';
-import { IconAppDelivery } from '../../../icons/icon-app-delivery';
 import { IconBeta } from '../../../icons/icon-beta';
 import { IconHangLoose } from '../../../icons/icon-hang-loose';
-import { IconHeartBox } from '../../../icons/icon-heart-box';
-import { IconShareBig } from '../../../icons/icon-share-big';
+import { IconSetLocation } from '../../../icons/icon-set-location';
 
 export interface ScreenConfig {
   icon: React.ReactNode;
@@ -46,36 +44,41 @@ export const consumer: ScreenConfig[] = [
     header: t('O AppJusto ainda está\n em fase de testes'),
     body: [
       t(
-        'Estamos em fase de testes na plataforma para construir o melhor serviço para você. Fique à vontade para contribuir com sugestões sobre melhorias e problemas.'
+        'Estamos em fase de testes para construir o melhor serviço para você. Logo, logo liberaremos o app e você poderá participar de uma economia mais justa.'
       ),
     ],
     icon: <IconBeta />,
   },
   {
-    header: t('Use para pedir comida e enviar\n encomendas'),
-    body: [
-      t(
-        'No Delivery de comida, o entregador recebe o valor integral da corrida e cobramos a menor taxa dos restaurantes. Na entrega de encomendas, os entregadores recebem o valor da frota que você escolher.'
-      ),
-    ],
-    icon: <IconAppDelivery />,
+    header: t('Em qual cidade você mora?'),
+    body: [t('Informaremos assim que o app estiver disponível perto de você!')],
+    icon: <IconSetLocation />,
   },
-  {
-    header: t('Escolha a frota que quiser!'),
-    body: [
-      t(
-        'No AppJusto, os entregadores podem criar frotas com as suas próprias condições. Você escolhe a que mais se identificar! Elas são ordenadas pela quantidade de entregadores perto de você.'
-      ),
-    ],
-    icon: <IconHeartBox />,
-  },
-  {
-    header: t('Use o AppJusto e indique para\n os seus amigos'),
-    body: [
-      t(
-        'Quanto mais usar e indicar, mais você ajuda esse movimento a crescer. Contribua para um delivery mais justo na sua região! Vamos começar?'
-      ),
-    ],
-    icon: <IconShareBig />,
-  },
+  // {
+  //   header: t('Use para pedir comida e enviar\n encomendas'),
+  //   body: [
+  //     t(
+  //       'No Delivery de comida, o entregador recebe o valor integral da corrida e cobramos a menor taxa dos restaurantes. Na entrega de encomendas, os entregadores recebem o valor da frota que você escolher.'
+  //     ),
+  //   ],
+  //   icon: <IconAppDelivery />,
+  // },
+  // {
+  //   header: t('Escolha a frota que quiser!'),
+  //   body: [
+  //     t(
+  //       'No AppJusto, os entregadores podem criar frotas com as suas próprias condições. Você escolhe a que mais se identificar! Elas são ordenadas pela quantidade de entregadores perto de você.'
+  //     ),
+  //   ],
+  //   icon: <IconHeartBox />,
+  // },
+  // {
+  //   header: t('Use o AppJusto e indique para\n os seus amigos'),
+  //   body: [
+  //     t(
+  //       'Quanto mais usar e indicar, mais você ajuda esse movimento a crescer. Contribua para um delivery mais justo na sua região! Vamos começar?'
+  //     ),
+  //   ],
+  //   icon: <IconShareBig />,
+  // },
 ];
