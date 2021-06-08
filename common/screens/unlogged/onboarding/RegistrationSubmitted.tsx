be16d5d1f421144f7f6491abdc632bdbdc584934
Daniel Lima <danielfbl@gmail.com>
2021-06-08 16:58:08 -0300

    populating the RegistrationSubmitted screen. work in progress

--- common/screens/unlogged/onboarding/RegistrationSubmitted.tsx
+++ common/screens/unlogged/onboarding/RegistrationSubmitted.tsx
@@ -5,8 +5,11 @@ import { Text } from 'react-native';
 import { ScrollView } from 'react-native-gesture-handler';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { LoggedNavigatorParamList } from '../../../../consumer/v2/types';
+import { t } from '../../../../strings';
 import PaddedView from '../../../components/containers/PaddedView';
-import { screens } from '../../../styles';
+import RoundedText from '../../../components/texts/RoundedText';
+import { colors, padding, screens, texts } from '../../../styles';
+import HomeShareCard from '../../home/cards/HomeShareCard';
 import { SocialMediaCard } from '../../home/cards/SocialMediaCard';
 
 type ScreenNavigationProp = StackNavigationProp<LoggedNavigatorParamList, 'RegistrationSubmitted'>;
@@ -18,11 +21,31 @@ type Props = {
 };
 
 export const RegistrationSubmitted = ({ navigation, route }: Props) => {
+  // this screen must receive the city: string from the Onboarding screen
   return (
     <SafeAreaView style={{ ...screens.config }}>
       <ScrollView contentContainerStyle={{ flexGrow: 1 }}>
+        <PaddedView style={{ backgroundColor: colors.green500 }}>
+          <RoundedText>{t('Pré-cadastro realizado com sucesso!')}</RoundedText>
+          {/* add a "user counter" in this text. if user counter < 10, replace
+          the string with 'Obrigado por fazer parte desse movimento em {nome da cidade}' */}
+          <Text style={{ ...texts.x2l, marginVertical: padding }}>
+            {t('Você e mais 000 pessoas já fazem parte desse movimento em {nome da cidade}')}
+          </Text>
+          <Text style={{ ...texts.md, marginBottom: 24 }}>
+            {t(
+              'Quanto mais gente compartilhar essa ideia, mais rápido um delivery justo para todos será realidade. Participe divulgando!'
+            )}
+          </Text>
+          <HomeShareCard
+            title="Divulgue o AppJusto"
+            subtitle="Clique para compartilhar o movimento nas suas redes"
+          />
+        </PaddedView>
         <PaddedView>
-          <Text>CONSUMER PROFILE SUBMITTED SCREEN</Text>
+          <Text style={{ ...texts.lg, marginBottom: 24 }}>
+            {t('Enquanto isso, aproveite para conhecer mais sobre o AppJusto:')}
+          </Text>
           <SocialMediaCard app="instagram" />
         </PaddedView>
       </ScrollView>
