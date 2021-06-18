c8ffe6ec057640393ff66541fa82e311cef1fa27
Daniel Lima <danielfbl@gmail.com>
2021-06-18 10:20:47 -0300

    Adjusting the OngoingOrderStatus for p2p orders

--- consumer/v2/ongoing/OngoingOrderStatus.tsx
+++ consumer/v2/ongoing/OngoingOrderStatus.tsx
@@ -58,53 +58,42 @@ export const OngoingOrderStatus = ({ order }: Props) => {
     } else if (status === 'canceled') {
       header = t('Pedido cancelado!');
       description = '';
+    } else if (status === 'declined') {
+      header = t('Problemas no pagamento');
+      description = '';
+    }
+  } else if (type === 'p2p') {
+    if (status === 'confirmed') {
+      header = t('Pedido aprovado!');
+      description = t('Aguarde enquanto procuramos um entregador para você.');
+    } else if (status === 'dispatching') {
+      if (dispatchingState === 'going-pickup') {
+        header = t('Indo para a coleta');
+        description = t('O entregador está indo para o local de coleta.');
+      } else if (dispatchingState === 'arrived-pickup') {
+        header = t('Aguardando coleta');
+        description = t('O entregador já chegou ao local de coleta.');
+      } else if (dispatchingState === 'going-destination') {
+        header = t('Saiu para entrega');
+        description = t(
+          'Já pode se preparar! O entregador saiu e já está levando a encomenda ao destino.'
+        );
+      } else if (dispatchingState === 'arrived-destination') {
+        header = t('Entregador chegou!');
+        description = t('O entregador já está no local de entrega.');
+      }
+    } else if (status === 'delivered') {
+      header = t('Pedido entregue!');
+      description = '';
+    } else if (status === 'canceled') {
+      header = t('Pedido cancelado!');
+      description = '';
+    } else if (status === 'declined') {
+      header = t('Problemas no pagamento');
+      description = '';
     }
   }
 
-  // if (status === 'confirming') {
-  // } else if (status === 'confirmed') {
-  //   header = t('Pedido aprovado!');
-  //   description =
-  //     order.type === 'food'
-  //       ? t('Aguarde enquanto o restaurante confirma seu pedido.')
-  //       : t('Aguarde enquanto procuramos um entregador para você.');
-  // } else if (status === 'preparing') {
-  //   header = t('Pedido em preparo');
-  //   description = t(
-  //     'O restaurante começou a preparar seu pedido e logo estará prontinho para você.'
-  //   );
-  // } else if (status === 'ready') {
-  //   header = t('Pronto para entrega');
-  //   description = t('Estamos aguardando o entregador pegar o seu pedido e levá-lo até você.');
-  // } else if (status === 'dispatching') {
-  //   if (dispatchingState === 'going-pickup') {
-  //     header = order.type === 'food' ? t('Indo para o restaurante') : t('Indo para a coleta');
-  //     description =
-  //       order.type === 'food'
-  //         ? t('O entregador já está indo pegar o pedido.')
-  //         : t('O entregador está indo para o local de coleta.');
-  //   } else if (dispatchingState === 'arrived-pickup') {
-  //     header = order.type === 'food' ? t('Aguardando retirada') : t('Aguardando coleta');
-  //     description =
-  //       order.type === 'food'
-  //         ? t('O entregador já chegou ao restaurante.')
-  //         : t('O entregador já chegou ao local de coleta.');
-  //   } else if (dispatchingState === 'going-destination') {
-  //     header = t('Saiu para entrega');
-  //     description = t('Já pode se preparar! O entregador saiu e está levando o pedido até você.');
-  //   } else if (dispatchingState === 'arrived-destination') {
-  //     header = t('Entregador chegou!');
-  //     description = t('O entregador já está no local de entrega.');
-  //   }
-  // } else if (status === 'delivered') {
-  //   header = t('Pedido entregue!');
-  //   description = '';
-  // } else if (status === 'canceled') {
-  //   header = t('Pedido cancelado!');
-  //   description = '';
-  // } else if (status === 'declined') {
-  // }
-  // if (header === null || description === null) return null;
   return (
     <View style={{ paddingHorizontal: padding, alignItems: 'center', paddingBottom: padding }}>
       {status === 'dispatching' && dispatchingState === 'arrived-destination' ? (
