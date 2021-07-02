57495a40c1c713d40e2b7f3388a357348595e5de
Daniel Lima <danielfbl@gmail.com>
2021-07-02 09:15:09 -0300

    removed R$1,00 tip

--- consumer/v2/common/TipControl.tsx
+++ consumer/v2/common/TipControl.tsx
@@ -20,7 +20,6 @@ type Props = {
 
 const data: HorizontalSelectItem[] = [
   { id: '0', title: t('Sem caixinha'), data: 0 },
-  { id: '1', title: formatCurrency(100), data: 100 },
   { id: '3', title: formatCurrency(300), data: 300 },
   { id: '5', title: formatCurrency(500), data: 500 },
   { id: '8', title: formatCurrency(800), data: 800 },
