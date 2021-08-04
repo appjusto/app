6c971ad26fb3b571d80a8d2d8f3f9509f6ea4e21
Daniel Lima <danielfbl@gmail.com>
2021-08-04 11:58:13 -0300

    finished the OrderCanceled screen with the correct "messages" depending on the case; added a navigation.replace to OrderCanceled from OngoingOrder for when a restaurant cancels an order; added a replace navigation action to the cancelOrderHandler in the OngoingOrderConfirmCancel to move the user from the OngoingOrder stack when he cancels an order (so the OrderCanceled screen won't appear)

--- courier/approved/ongoing/OrderCanceled.tsx
+++ courier/approved/ongoing/OrderCanceled.tsx
@@ -2,10 +2,13 @@ import { CompositeNavigationProp, RouteProp } from '@react-navigation/core';
 import { StackNavigationProp } from '@react-navigation/stack';
 import React from 'react';
 import { ActivityIndicator, View } from 'react-native';
+import { useSelector } from 'react-redux';
 import DefaultButton from '../../../common/components/buttons/DefaultButton';
 import FeedbackView from '../../../common/components/views/FeedbackView';
 import { IconConeYellow } from '../../../common/icons/icon-cone-yellow';
 import { useObserveOrder } from '../../../common/store/api/order/hooks/useObserveOrder';
+import { useOrderCancellationInfo } from '../../../common/store/api/order/hooks/useOrderCancellationInfo';
+import { getFlavor } from '../../../common/store/config/selectors';
 import { colors, padding, screens } from '../../../common/styles';
 import { formatCurrency } from '../../../common/utils/formatters';
 import { OngoingOrderNavigatorParamList } from '../../../consumer/v2/ongoing/types';
@@ -39,20 +42,40 @@ export default ({ navigation, route }: Props) => {
   const { orderId } = route.params;
   // screen state
   const order = useObserveOrder(orderId);
+  const cancelInfo = useOrderCancellationInfo(orderId);
+  const flavor = useSelector(getFlavor);
   // UI
-  if (!order) {
+  if (!order || !cancelInfo) {
     return (
       <View style={screens.centered}>
         <ActivityIndicator size="large" color={colors.green500} />
       </View>
     );
   }
+  // helpers
+  const courierFeeRefunded = cancelInfo.params.refund.includes('delivery');
+  const description = (() => {
+    if (flavor === 'courier') {
+      if (courierFeeRefunded) return undefined;
+      else return t('Como você já iniciou o pedido, você receberá: ');
+    }
+    if (flavor === 'consumer') {
+      return `${t('Esse pedido foi cancelado por')} ${order.business!.name}${t(
+        '. A cobrança será estornada.'
+      )}`;
+    }
+  })();
+  const value = (() => {
+    if (flavor === 'courier' && !courierFeeRefunded)
+      return formatCurrency(order.fare!.courier.value);
+    else return undefined;
+  })();
   return (
     <FeedbackView
       header={t('Esse pedido foi cancelado')}
       icon={<IconConeYellow />}
-      description={t('Como você já iniciou o pedido, você receberá: ')}
-      value={formatCurrency(order.fare!.courier.value)}
+      description={description}
+      value={value}
     >
       <View style={{ marginBottom: padding }}>
         <DefaultButton
