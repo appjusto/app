73d7350b1b31378df00b5e0786f2c9ebeeccbe35
Daniel Lima <danielfbl@gmail.com>
2021-08-17 16:18:06 -0300

    deleted comment

--- courier/approved/main/history/MarketplaceAccountInfo.tsx
+++ courier/approved/main/history/MarketplaceAccountInfo.tsx
@@ -170,7 +170,6 @@ export const MarketplaceAccountInfo = () => {
             <DefaultButton
               style={{ marginTop: padding }}
               title={t('Antecipar valores')}
-              // disabled={receivableBalance === 0}
               onPress={advanceHandler}
               secondary
             />
