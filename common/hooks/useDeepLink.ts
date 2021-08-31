bdc76ccc3b9afb2b2d249163c192cf462ce8becd
Pedro Andrade <pdandradeb@gmail.com>
2021-08-31 08:53:38 -0300

    Tracking deeplink only in non live environments

--- common/hooks/useDeepLink.ts
+++ common/hooks/useDeepLink.ts
@@ -1,24 +1,32 @@
 import * as Linking from 'expo-linking';
 import React from 'react';
+import { useSelector } from 'react-redux';
 import { track } from '../store/api/track';
+import { getExtra } from '../utils/config';
 
 export const useDeeplink = () => {
+  // redux
+  const { environment } = useSelector(getExtra);
   // state
   const [deeplink, setDeeplink] = React.useState<string | null>();
   // side effects
   // once
   React.useEffect(() => {
     Linking.getInitialURL().then((value) => {
-      track('Initial Deeplink', {
-        url: value,
-      });
+      if (environment !== 'live') {
+        track('Initial Deeplink', {
+          url: value,
+        });
+      }
       setDeeplink(value);
     });
     const handler: Linking.URLListener = (ev) => {
       setDeeplink(ev.url);
-      track('Deeplink changed', {
-        url: ev.url,
-      });
+      if (environment !== 'live') {
+        track('Deeplink changed', {
+          url: ev.url,
+        });
+      }
     };
     Linking.addEventListener('url', handler);
 
