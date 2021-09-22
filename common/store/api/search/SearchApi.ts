ed09d8474d4475fcd9126563e3d8b2ed0e20c4ae
Pedro Andrade <pdandradeb@gmail.com>
2021-09-22 19:52:46 -0300

    Updating default search results for 30 results per page

--- common/store/api/search/SearchApi.ts
+++ common/store/api/search/SearchApi.ts
@@ -71,7 +71,7 @@ export default class SearchApi {
     aroundLocation: LatLng,
     query: string = '',
     page?: number,
-    hitsPerPage: number = 10
+    hitsPerPage: number = 30
   ) {
     const index = this.getSearchIndex(kind, order);
     if (!index) throw new Error('Invalid index');
