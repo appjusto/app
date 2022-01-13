e5e361810f2febd03ad1692bcda1a9bd06522603
Pedro Andrade <pdandradeb@gmail.com>
2022-01-13 11:18:11 -0300

    Disabling place order button if route has issue

--- consumer/v2/food/restaurant/checkout/FoodOrderCheckout.tsx
+++ consumer/v2/food/restaurant/checkout/FoodOrderCheckout.tsx
@@ -65,10 +65,11 @@ export const FoodOrderCheckout = ({ navigation, route }: Props) => {
   const [shareDataWithBusiness, setShareDataWithBusiness] = React.useState(false);
   const { quotes, getOrderQuotes } = useQuotes(order?.id);
   const [selectedFare, setSelectedFare] = React.useState<Fare>();
-  const canSubmit = React.useMemo(() => {
-    return selectedPaymentMethodId !== undefined && selectedFare !== undefined && !isLoading;
-  }, [selectedPaymentMethodId, selectedFare, isLoading]);
-
+  const canSubmit =
+    selectedPaymentMethodId !== undefined &&
+    selectedFare !== undefined &&
+    !isLoading &&
+    isEmpty(order?.route?.issue);
   // side effects
   // whenever quotes are updated
   // select first fare
