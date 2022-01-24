b7b3f106060ba0c77f453e7f81a53d1a714ac6e1
Daniel Lima <danielfbl@gmail.com>
2022-01-24 16:58:45 -0300

    more layout. wip

--- courier/approved/matching/Layout.tsx
+++ courier/approved/matching/Layout.tsx
@@ -2,9 +2,12 @@ import { RouteProp } from '@react-navigation/native';
 import { StackNavigationProp } from '@react-navigation/stack';
 import React from 'react';
 import { ScrollView, Text, View } from 'react-native';
+import useTallerDevice from '../../../common/hooks/useTallerDevice';
+import { useSegmentScreen } from '../../../common/store/api/track';
 import { colors, halfPadding, padding, screens, texts } from '../../../common/styles';
 import { LoggedNavigatorParamList } from '../../../consumer/v2/types';
 import { t } from '../../../strings';
+import { AcceptControl } from './AcceptControl';
 
 type ScreenNavigationProp = StackNavigationProp<LoggedNavigatorParamList, 'Layout'>;
 type ScreenRouteProp = RouteProp<LoggedNavigatorParamList, 'Layout'>;
@@ -15,6 +18,10 @@ type Props = {
 };
 
 export const Layout = () => {
+  //helpers
+  const tallerDevice = useTallerDevice();
+  // tracking
+  useSegmentScreen('Matching');
   return (
     <ScrollView
       style={[screens.default]}
@@ -39,15 +46,49 @@ export const Layout = () => {
           </Text>
           <Text style={{ ...texts.md, color: colors.grey700 }}>{t('Nova corrida disponível')}</Text>
         </View>
-        <View style={{ marginTop: 24, width: '100%', height: 64, flexDirection: 'row' }}>
-          <View>
-            <Text style={{ ...texts.md, color: colors.grey700 }}>{t('Você recebe')}</Text>
-            <Text style={{ ...texts.x4l }}>R$ 12,50</Text>
+        <View
+          style={{
+            marginTop: 24,
+            width: '100%',
+            height: 64,
+            flexDirection: 'row',
+            justifyContent: 'space-between',
+          }}
+        >
+          <View style={{ alignItems: 'center', flex: 1 }}>
+            <Text style={{ ...texts.md, color: colors.grey700, textAlign: 'center' }}>
+              {t('Você recebe')}
+            </Text>
+            <Text style={{ ...texts.x4l, textAlign: 'center' }}>R$ 12,50</Text>
           </View>
-          <View>
-            <Text style={{ ...texts.md, color: colors.grey700 }}>{t('Você recebe')}</Text>
-            <Text style={{ ...texts.x4l }}>R$ 12,50</Text>
+          <View
+            style={{
+              height: '100%',
+              borderLeftColor: colors.grey500,
+              borderLeftWidth: 1,
+              borderStyle: 'solid',
+            }}
+          />
+          <View style={{ alignItems: 'center', flex: 1 }}>
+            <Text style={{ ...texts.md, color: colors.grey700, textAlign: 'center' }}>
+              {t('Retirada em')}
+            </Text>
+            <Text style={{ ...texts.x4l, textAlign: 'center' }}>3.4 KM</Text>
           </View>
+        </View>
+        <View style={{ flex: 1 }} />
+        {/* cards origin and destination */}
+        <View style={{ flex: 1 }} />
+        {/* slider */}
+        <View>
+          <AcceptControl
+            onAccept={() => null}
+            onReject={() => null}
+            style={{
+              marginBottom: tallerDevice ? padding * 4 : padding,
+              paddingHorizontal: padding,
+            }}
+          />
         </View>
       </View>
     </ScrollView>
