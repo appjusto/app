7a217db716d174e8fc3ef51eea3674beab0a2411
Daniel Lima <danielfbl@gmail.com>
2022-01-28 15:02:29 -0300

    more refactoring. wip

--- consumer/v2/common/review/ReviewBox.tsx
+++ consumer/v2/common/review/ReviewBox.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import { Text, TextInputProps, View } from 'react-native';
 import DefaultInput from '../../../../common/components/inputs/DefaultInput';
 import SingleHeader from '../../../../common/components/texts/SingleHeader';
+import HR from '../../../../common/components/views/HR';
 import { colors, halfPadding, padding, texts } from '../../../../common/styles';
 import { t } from '../../../../strings';
 import { ThumbSelector } from './ThumbSelector';
@@ -25,20 +26,35 @@ export const ReviewBox = ({
     <View
       style={{
         flex: 1,
+        backgroundColor: colors.white,
+        paddingBottom: padding,
       }}
     >
       <SingleHeader title={t('Avalie sua experiência')} />
       <ThumbSelector
-        title={t('Como foi a sua experiência com o entregador?')}
+        title="Entregador"
+        iconUnicode={0x1f6f5}
         review={reviewType}
         onReviewChange={onReviewChange}
       />
+      <ThumbSelector
+        title="Restaurante"
+        iconUnicode={0x1f373}
+        review={reviewType}
+        onReviewChange={onReviewChange}
+      />
+      <ThumbSelector
+        title="AppJusto"
+        iconUnicode={0x1f4f1}
+        review={reviewType}
+        onReviewChange={onReviewChange}
+      />
+      <HR height={padding} style={{ backgroundColor: colors.grey50 }} />
       <View>
+        <SingleHeader title={t('Deixe um comentário')} />
         <View style={{ paddingHorizontal: padding, paddingBottom: halfPadding }}>
           <Text style={{ ...texts.md, color: colors.grey700, paddingVertical: halfPadding }}>
-            {t(
-              'Se preferir, descreva a sua experiência para outros clientes. Sua avaliação será anônima.'
-            )}
+            {t('Se preferir, descreva a sua experiência de forma anônima.')}
           </Text>
           <DefaultInput
             editable={editable}
--- consumer/v2/common/review/ThumbSelector.tsx
+++ consumer/v2/common/review/ThumbSelector.tsx
@@ -1,69 +1,80 @@
 import { ReviewType } from '@appjusto/types';
 import { Feather } from '@expo/vector-icons';
-import { t } from 'i18n-js';
 import React from 'react';
 import { Text, TouchableWithoutFeedback, View } from 'react-native';
 import PaddedView from '../../../../common/components/containers/PaddedView';
 import { borders, colors, halfPadding, padding, texts } from '../../../../common/styles';
+import { t } from '../../../../strings';
 
 type Props = {
+  title: string;
+  iconUnicode: number;
   review?: ReviewType;
   onReviewChange?: (type: ReviewType) => void;
 };
 
-export const ThumbSelector = ({ review, onReviewChange }: Props) => {
+export const ThumbSelector = ({ title, iconUnicode, review, onReviewChange }: Props) => {
   return (
-    <PaddedView>
+    <PaddedView style={{ flex: 1 }}>
       <View
         style={{
           flexDirection: 'row',
           alignItems: 'center',
+          justifyContent: 'space-between',
         }}
       >
-        <View>
-          <Text style={{...texts.xl}}>{String.fromCodePoint(U+1F9BD)}<Text style={{marginLeft: halfPadding}}>{t('Entregador')}</Text></Text>
+        <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+          <Text style={{ ...texts.xl }}>{String.fromCodePoint(iconUnicode)}</Text>
+          <Text style={{ ...texts.xl, marginLeft: halfPadding }}>{t(title)}</Text>
         </View>
-        <TouchableWithoutFeedback
-          onPress={() => {
-            if (onReviewChange) onReviewChange('positive');
+        <View
+          style={{
+            flexDirection: 'row',
+            alignItems: 'center',
           }}
         >
-          <View
-            style={{
-              height: 64,
-              width: 64,
-              ...borders.default,
-              borderRadius: 32,
-              borderColor: colors.green500,
-              backgroundColor: review === 'positive' ? colors.green500 : colors.red,
-              justifyContent: 'center',
-              alignItems: 'center',
+          <TouchableWithoutFeedback
+            onPress={() => {
+              if (onReviewChange) onReviewChange('positive');
             }}
           >
-            <Feather name="thumbs-up" size={24} />
-          </View>
-        </TouchableWithoutFeedback>
-        <TouchableWithoutFeedback
-          onPress={() => {
-            if (onReviewChange) onReviewChange('negative');
-          }}
-        >
-          <View
-            style={{
-              height: 64,
-              width: 64,
-              ...borders.default,
-              borderRadius: 32,
-              borderColor: colors.green500,
-              marginLeft: padding,
-              backgroundColor: review === 'negative' ? colors.green500 : colors.white,
-              justifyContent: 'center',
-              alignItems: 'center',
+            <View
+              style={{
+                height: 64,
+                width: 64,
+                ...borders.default,
+                borderRadius: 32,
+                borderColor: colors.green500,
+                backgroundColor: review === 'positive' ? colors.green500 : colors.red,
+                justifyContent: 'center',
+                alignItems: 'center',
+              }}
+            >
+              <Feather name="thumbs-up" size={24} />
+            </View>
+          </TouchableWithoutFeedback>
+          <TouchableWithoutFeedback
+            onPress={() => {
+              if (onReviewChange) onReviewChange('negative');
             }}
           >
-            <Feather name="thumbs-down" size={24} />
-          </View>
-        </TouchableWithoutFeedback>
+            <View
+              style={{
+                height: 64,
+                width: 64,
+                ...borders.default,
+                borderRadius: 32,
+                borderColor: colors.green500,
+                marginLeft: padding,
+                backgroundColor: review === 'negative' ? colors.green500 : colors.white,
+                justifyContent: 'center',
+                alignItems: 'center',
+              }}
+            >
+              <Feather name="thumbs-down" size={24} />
+            </View>
+          </TouchableWithoutFeedback>
+        </View>
       </View>
     </PaddedView>
   );
