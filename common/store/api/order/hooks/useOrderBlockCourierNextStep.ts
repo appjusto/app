1b9238ee7ac2f0409c8bb0e26cb1d070841193d5
Pedro Andrade <pdandradeb@gmail.com>
2022-02-16 08:24:43 -0300

    Using dispatchingTimestamps to block next step

--- common/store/api/order/hooks/useOrderBlockCourierNextStep.ts
+++ common/store/api/order/hooks/useOrderBlockCourierNextStep.ts
@@ -1,41 +1,52 @@
 import React from 'react';
 import { ApiContext } from '../../../../app/context';
 import { usePlatformParamsContext } from '../../../../contexts/PlatformParamsContext';
+import { useContextGetSeverTime } from '../../../../contexts/ServerTimeContext';
 import { useObserveOrder } from './useObserveOrder';
-import { useOrderDispatchingStateElapsedSeconds } from './useOrderDispatchingStateElapsedSeconds';
+
+const canAdvanceDispatchingState = (
+  now: Date,
+  delayBeforeAdvancing: number,
+  currentStateTimestamp: firebase.firestore.FieldValue | undefined
+) => {
+  if (!currentStateTimestamp) return false;
+  const timestamp = (currentStateTimestamp as firebase.firestore.Timestamp).toDate();
+  const diff = now.getTime() - timestamp.getTime();
+  if (diff === 0) return false;
+  return diff >= delayBeforeAdvancing * 1000;
+};
 
 export const useOrderBlockCourierNextStep = (orderId: string) => {
   // context
   const api = React.useContext(ApiContext);
+  const getServerTime = useContextGetSeverTime()!;
   const delayBeforeAdvancing = usePlatformParamsContext()?.courier.delayBeforeAdvancing ?? 60;
   // state
-  const secondsSinceGoingPickup = useOrderDispatchingStateElapsedSeconds(orderId, 'going-pickup');
-  const secondsSinceGoingDestination = useOrderDispatchingStateElapsedSeconds(
-    orderId,
-    'going-destination'
-  );
   const order = useObserveOrder(orderId);
   const [blockNextStep, setBlockNextStep] = React.useState(false);
   // side effects
   React.useEffect(() => {
-    if (!delayBeforeAdvancing) return;
     if (!order) return;
-    const { status, dispatchingState, type } = order;
+    const { type, status, dispatchingState, dispatchingTimestamps } = order;
+    const now = getServerTime();
     if (dispatchingState === 'going-pickup') {
       setBlockNextStep(
-        secondsSinceGoingPickup !== undefined && secondsSinceGoingPickup < delayBeforeAdvancing
+        canAdvanceDispatchingState(now, delayBeforeAdvancing, dispatchingTimestamps.goingPickup)
       );
     } else if (dispatchingState === 'arrived-pickup') {
       setBlockNextStep(type === 'food' && status !== 'dispatching');
     } else if (dispatchingState === 'going-destination') {
       setBlockNextStep(
-        secondsSinceGoingDestination !== undefined &&
-          secondsSinceGoingDestination < delayBeforeAdvancing
+        canAdvanceDispatchingState(
+          now,
+          delayBeforeAdvancing,
+          dispatchingTimestamps.goingDestination
+        )
       );
     } else {
       setBlockNextStep(false);
     }
-  }, [delayBeforeAdvancing, api, order, secondsSinceGoingPickup, secondsSinceGoingDestination]);
+  }, [order, delayBeforeAdvancing]);
   // result
   return blockNextStep;
 };
