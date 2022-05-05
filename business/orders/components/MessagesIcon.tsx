09745755ab2f6b5b7d00093777834331919a57ee
Daniel Lima <danielfbl@gmail.com>
2022-05-05 14:01:44 -0300

    fixed elements positions in the MessagesIcon component

--- business/orders/components/MessagesIcon.tsx
+++ business/orders/components/MessagesIcon.tsx
@@ -11,28 +11,27 @@ type Props = {
 export const MessagesIcon = ({ onPress, unreadMessages }: Props) => {
   return (
     <TouchableOpacity onPress={onPress}>
-      {unreadMessages > 0 ? (
-        <View
-          style={{
-            height: 20,
-            width: 20,
-            justifyContent: 'center',
-            alignItems: 'center',
-            borderRadius: 10,
-            backgroundColor: colors.red,
-            // top: -8,
-            top: -6,
-            left: 12,
-            position: 'absolute',
-          }}
-        >
-          <Text style={{ color: colors.white }}>{unreadMessages}</Text>
-        </View>
-      ) : null}
       <View>
         <View>
           <Feather name="message-circle" size={28} color={colors.black} />
         </View>
+        {unreadMessages > 0 ? (
+          <View
+            style={{
+              height: 20,
+              width: 20,
+              justifyContent: 'center',
+              alignItems: 'center',
+              borderRadius: 10,
+              backgroundColor: colors.red,
+              top: -4,
+              left: 12,
+              position: 'absolute',
+            }}
+          >
+            <Text style={{ color: colors.white }}>{unreadMessages}</Text>
+          </View>
+        ) : null}
       </View>
     </TouchableOpacity>
   );
