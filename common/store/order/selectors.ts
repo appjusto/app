257f5eb9a685b22186cbb3b27b7397fceb3964c6
Daniel Lima <danielfbl@gmail.com>
2022-06-14 11:35:20 -0300

    fixing selector function in this branch

--- common/store/order/selectors.ts
+++ common/store/order/selectors.ts
@@ -72,12 +72,22 @@ export const summarizeOrders = memoize((orders: WithId<Order>[]) =>
       delivered: order.status === 'delivered' ? result.delivered + 1 : result.delivered,
       canceled: order.status === 'canceled' ? result.canceled + 1 : result.canceled,
       ongoing: isOrderOngoing(order) ? result.ongoing + 1 : result.ongoing,
+      quote: order.status === 'quote' ? result.quote + 1 : result.quote,
+      total:
+        order.status === 'delivered' ||
+        order.status === 'canceled' ||
+        isOrderOngoing(order) ||
+        order.status === 'quote'
+          ? result.total + 1
+          : result.total,
       courierFee:
         order.status === 'delivered'
-          ? result.courierFee + (order.fare?.courier.value ?? 0) + (order.tip?.value ?? 0)
+          ? result.courierFee +
+            (order.fare!.courier.value - order.fare!.courier.financialFee) +
+            ((order.tip?.value ?? 0) - (order.tip?.financialFee ?? 0))
           : result.courierFee,
     }),
-    { delivered: 0, canceled: 0, ongoing: 0, courierFee: 0 }
+    { delivered: 0, canceled: 0, ongoing: 0, quote: 0, total: 0, courierFee: 0 }
   )
 );
 
