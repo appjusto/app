de52a5f69cc9e1a89284d886bb88cf9e8c6cde83
Daniel Lima <danielfbl@gmail.com>
2022-07-07 13:23:24 -0300

    refactoring hook

--- courier/approved/main/history/invoices/useCourierInvoicesAmount.tsx
+++ courier/approved/main/history/invoices/useCourierInvoicesAmount.tsx
@@ -3,7 +3,11 @@ import dayjs from 'dayjs';
 import React from 'react';
 import { ApiContext } from '../../../../../common/app/context';
 
-export const useCourierInvoicesAmount = (courierId?: string, month?: Date | null) => {
+export const useCourierInvoicesAmount = (
+  kind: 'month' | 'week',
+  courierId?: string,
+  period?: Date | null
+) => {
   // context
   const api = React.useContext(ApiContext);
   // state
@@ -12,14 +16,20 @@ export const useCourierInvoicesAmount = (courierId?: string, month?: Date | null
   // side effects
   React.useEffect(() => {
     if (!courierId) return;
-    if (!month) return;
-    const start = dayjs(month).startOf('month').toDate();
-    const end = dayjs(month).endOf('month').toDate();
+    if (!period) return;
+    const start =
+      kind === 'month'
+        ? dayjs(period).startOf('month').toDate()
+        : dayjs(period).startOf('week').toDate();
+    const end =
+      kind === 'month'
+        ? dayjs(period).endOf('month').toDate()
+        : dayjs(period).endOf('week').toDate();
     const unsubscribe = api
       .courier()
       .observeCourierInvoices({ courierId, status: 'paid', start, end }, setInvoices);
     return () => unsubscribe();
-  }, [api, courierId, month]);
+  }, [api, courierId, period, kind]);
   React.useEffect(() => {
     if (!invoices) return;
     const amount = invoices.reduce((total, invoice) => {
