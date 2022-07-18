9cbf4b2d4f4bc210d33d7048f20d0a412a7b636a
Daniel Lima <danielfbl@gmail.com>
2022-07-18 16:32:46 -0300

    refactoring the OrderPayment component to reflect the new flow. wip

--- consumer/v2/common/order-summary/OrderPayment.tsx
+++ consumer/v2/common/order-summary/OrderPayment.tsx
@@ -4,14 +4,12 @@ import { Text, TouchableOpacity, View } from 'react-native';
 import { useSelector } from 'react-redux';
 import DefaultButton from '../../../../common/components/buttons/DefaultButton';
 import PaddedView from '../../../../common/components/containers/PaddedView';
-import RoundedText from '../../../../common/components/texts/RoundedText';
 import { getPaymentMethodById } from '../../../../common/store/api/business/consumer/selectors';
 import { useIsPixEnabled } from '../../../../common/store/api/order/ui/useIsPixEnabled';
 import { useProfileSummary } from '../../../../common/store/api/profile/useProfileSummary';
 import { getConsumer } from '../../../../common/store/consumer/selectors';
 import { borders, colors, halfPadding, padding, texts } from '../../../../common/styles';
 import { t } from '../../../../strings';
-import { PixCard } from '../PixCard';
 
 interface Props {
   selectedPaymentMethodId?: string;
@@ -42,91 +40,8 @@ export const OrderPayment = ({
   const payableWithPix = useIsPixEnabled();
   const { isProfileComplete, shouldVerifyPhone } = useProfileSummary();
   const selectedPaymentMethod = getPaymentMethodById(consumer, selectedPaymentMethodId);
-  // delete this after
-  const [interfaceNew, setInterfaceNew] = React.useState(true);
-  return !interfaceNew ? (
-    <View>
-      {/* <View style={{ marginTop: padding }}>
-        <SingleHeader title={t('Forma de pagamento')} />
-      </View> */}
-      <PaddedView>
-        {Boolean(!selectedPaymentMethod) ? (
-          <View style={{ marginBottom: padding }}>
-            <TouchableOpacity onPress={navigateToPayWithPix}>
-              <PixCard />
-            </TouchableOpacity>
-          </View>
-        ) : null}
-        {Boolean(selectedPaymentMethod) ? (
-          <View style={{ marginBottom: halfPadding }}>
-            <TouchableOpacity onPress={onEditPaymentMethod}>
-              <View style={{ marginBottom: halfPadding }}>
-                <View
-                  style={{
-                    flexDirection: 'row',
-                    justifyContent: 'space-between',
-                    alignItems: 'center',
-                  }}
-                >
-                  <Text style={{ ...texts.md }}>{t('Forma de pagamento')}</Text>
-                  <Feather
-                    name="edit-3"
-                    size={12}
-                    style={{ ...borders.default, borderColor: colors.grey50, padding: 8 }}
-                  />
-                </View>
-                <Text style={{ ...texts.sm, color: colors.grey700 }}>
-                  {`${t('Cartão de crédito')}: ${selectedPaymentMethod!.data.display_number}`}
-                </Text>
-              </View>
-            </TouchableOpacity>
-            <TouchableOpacity style={{ marginTop: halfPadding }} onPress={navigateToAboutCharges}>
-              <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-                <Feather name="info" size={14} />
-                <Text
-                  style={{ ...texts.xs, marginLeft: halfPadding, textDecorationLine: 'underline' }}
-                >
-                  {t('O valor total pode ser dividido em duas cobranças. Clique para saber mais')}
-                </Text>
-              </View>
-            </TouchableOpacity>
-          </View>
-        ) : null}
-
-        {isProfileComplete && selectedPaymentMethod && !activityIndicator ? (
-          <DefaultButton
-            title={shouldVerifyPhone ? t('Verificar telefone') : t('Confirmar pedido')}
-            onPress={onSubmit}
-            disabled={!isSubmitEnabled}
-            activityIndicator={activityIndicator}
-            style={{ marginTop: padding }}
-          />
-        ) : (
-          <DefaultButton
-            title={
-              !isProfileComplete ? t('Completar cadastro') : t('Escolher uma forma de pagamento')
-            }
-            onPress={onEditPaymentMethod}
-            variant="secondary"
-            style={{ marginTop: padding }}
-          />
-        )}
-
-        {payableWithPix && !activityIndicator ? (
-          <DefaultButton
-            variant="secondary"
-            title={t('Quero pagar com Pix')}
-            style={{ marginVertical: padding }}
-            onPress={navigateToPayWithPix}
-          >
-            <RoundedText backgroundColor={colors.darkYellow} style={{ right: -64 }}>
-              {t('Novo!')}
-            </RoundedText>
-          </DefaultButton>
-        ) : null}
-      </PaddedView>
-    </View>
-  ) : (
+  const canPlaceOrder = isProfileComplete && !shouldVerifyPhone;
+  return (
     <PaddedView>
       {Boolean(selectedPaymentMethod) ? (
         <View style={{ marginBottom: halfPadding }}>
@@ -163,13 +78,31 @@ export const OrderPayment = ({
           </TouchableOpacity>
         </View>
       ) : null}
-      {!isProfileComplete || shouldVerifyPhone ? (
+      {(!isProfileComplete || shouldVerifyPhone) && !activityIndicator ? (
         <DefaultButton
           variant="secondary"
           title={t('Completar cadastro')}
           style={{ marginVertical: padding }}
           onPress={navigateToCompleteProfile}
         />
+      ) : null}
+      {Boolean(selectedPaymentMethod) && canPlaceOrder && !activityIndicator ? (
+        <View>
+          <DefaultButton
+            variant="primary"
+            title={t('Confirmar pedido')}
+            style={{ marginTop: padding }}
+            onPress={onSubmit}
+          />
+          {payableWithPix ? (
+            <DefaultButton
+              variant="secondary"
+              title={t('Quero pagar com Pix')}
+              style={{ marginTop: padding }}
+              onPress={navigateToPayWithPix}
+            />
+          ) : null}
+        </View>
       ) : (
         <DefaultButton
           variant="secondary"
