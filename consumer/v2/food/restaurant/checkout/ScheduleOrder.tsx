2690bebe8a53eb9c77599c1ce7ab4ba93ca3d165
Daniel Lima <danielfbl@gmail.com>
2022-07-23 12:52:25 -0300

    ScheduleOrder: working the layout

--- consumer/v2/food/restaurant/checkout/ScheduleOrder.tsx
+++ consumer/v2/food/restaurant/checkout/ScheduleOrder.tsx
@@ -4,10 +4,16 @@ import { StackNavigationProp } from '@react-navigation/stack';
 import { Timestamp } from 'firebase/firestore';
 import { capitalize } from 'lodash';
 import React from 'react';
-import { ActivityIndicator, ScrollView, Text, TouchableOpacity, View } from 'react-native';
+import {
+  ActivityIndicator,
+  FlatList,
+  ScrollView,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
 import { ApiContext } from '../../../../../common/app/context';
 import CheckField from '../../../../../common/components/buttons/CheckField';
-import DefaultButton from '../../../../../common/components/buttons/DefaultButton';
 import { DayBoxListItem } from '../../../../../common/components/list items/DayBoxListItem';
 import { useContextGetSeverTime } from '../../../../../common/contexts/ServerTimeContext';
 import { useObserveBusiness } from '../../../../../common/store/api/business/hooks/useObserveBusiness';
@@ -41,11 +47,23 @@ export const ScheduleOrder = ({ navigation, route }: Props) => {
   const api = React.useContext(ApiContext);
   const now = getServerTime();
   const business = useObserveBusiness(order?.business?.id);
+  // helpers
+  const realTimeDelivery =
+    business?.status === 'open' && business?.preparationModes?.includes('realtime');
+  const daySchedules = business?.schedules ? scheduleFromDate(business.schedules, now) : [];
+  const nextDateSlots: Date[][] = getNextDateSlots(daySchedules, now, 60);
   // state
-  const [selectedDay, setSelectedDay] = React.useState<Date[]>();
+  const [selectedDay, setSelectedDay] = React.useState<Date[]>(nextDateSlots[0] ?? undefined);
   const [selectedSlot, setSelectedSlot] = React.useState<Timestamp>();
   const [loading, setLoading] = React.useState(false);
+  // side effects
+  // React.useEffect(() => {
+  //   if (business?.schedules) {
+  //     setSelectedDay(nextDateSlots[0]);
+  //   }
+  // }, [business?.schedules]);
 
+  // UI
   if (!order) return null; // shouldn't happen
   if (!business) {
     return (
@@ -54,44 +72,110 @@ export const ScheduleOrder = ({ navigation, route }: Props) => {
       </View>
     );
   }
-  const { schedules, status, preparationModes } = business;
-  const realTimeDelivery = status === 'open' && preparationModes?.includes('realtime');
-  const daySchedules = scheduleFromDate(schedules, now);
-  const nextDateSlots: Date[][] = realTimeDelivery
-    ? getNextDateSlots(daySchedules, now, 60)
-    : getNextDateSlots(daySchedules, now, 60).slice(1);
+
   return (
     <View style={{ ...screens.default, padding }}>
-      <View style={{ flex: 0.5 }}>
-        <ScrollView horizontal showsHorizontalScrollIndicator={false} style={{ flex: 0.5 }}>
-          {nextDateSlots.map((day, i) => (
-            <View style={{ marginRight: padding }} key={i}>
-              <DayBoxListItem
-                weekDay={capitalize(
-                  Dayjs(day[0]).calendar(now, {
-                    sameDay: '[hoje]',
-                    nextDay: 'dddd'.slice(0, 3),
-                    nextWeek: 'dddd'.slice(0, 3),
-                  })
-                )}
-                day={capitalize(
-                  Dayjs(day[0]).calendar(now, {
-                    sameDay: '[hoje]',
-                    nextDay: 'dddd'.slice(0, 3),
-                    nextWeek: 'dddd'.slice(0, 3),
-                  })
-                )}
-                selected={
-                  selectedDay !== undefined &&
-                  Timestamp.fromDate(day[0]).isEqual(Timestamp.fromDate(selectedDay[0]))
-                }
-                onSelect={() => setSelectedDay(day)}
-              />
-            </View>
-          ))}
+      <View style={{ flex: 1 }}>
+        <ScrollView
+          horizontal
+          showsHorizontalScrollIndicator={false}
+          style={{ flex: 1 }}
+          contentContainerStyle={{ flexGrow: 1 }}
+        >
+          {nextDateSlots.map((day, i) => {
+            return (
+              <View style={{ marginRight: padding }} key={i}>
+                <DayBoxListItem
+                  weekDay={capitalize(
+                    Dayjs(day[0]).calendar(now, {
+                      sameDay: '[hoje]',
+                      nextDay: 'dddd'.slice(0, 3),
+                      nextWeek: 'dddd'.slice(0, 3),
+                    })
+                  )}
+                  day={capitalize(
+                    Dayjs(day[0]).calendar(now, {
+                      sameDay: '[hoje]',
+                      nextDay: 'dddd'.slice(0, 3),
+                      nextWeek: 'dddd'.slice(0, 3),
+                    })
+                  )}
+                  // selected={
+                  //   selectedDay !== undefined &&
+                  //   Timestamp.fromDate(day[0]).isEqual(Timestamp.fromDate(selectedDay[0]))
+                  // }
+                  selected={
+                    Boolean(day) &&
+                    Boolean(selectedDay) &&
+                    day.toString() === selectedDay.toString()
+                  }
+                  onSelect={() => setSelectedDay(day)}
+                />
+              </View>
+            );
+          })}
         </ScrollView>
       </View>
-      {realTimeDelivery ? (
+      <View style={{ flex: 3 }}>
+        <FlatList
+          ListHeaderComponent={
+            realTimeDelivery ? (
+              <View style={{ flex: 0.5 }}>
+                <Text style={{ ...texts.md }}>{t('Entregar hoje')}</Text>
+                <TouchableOpacity onPress={() => navigation.navigate('FoodOrderCheckout')}>
+                  <View
+                    style={{
+                      flexDirection: 'row',
+                      alignItems: 'center',
+                      justifyContent: 'space-between',
+                      marginTop: halfPadding,
+                    }}
+                  >
+                    {order.arrivals?.destination?.estimate ? (
+                      <Text style={{ ...texts.sm, color: colors.grey700, marginTop: halfPadding }}>
+                        {getETAWithMargin(order.arrivals.destination.estimate)}
+                      </Text>
+                    ) : null}
+                    <CheckField checked={!order.scheduledTo} />
+                  </View>
+                </TouchableOpacity>
+              </View>
+            ) : null
+          }
+          data={selectedDay}
+          keyExtractor={(item) => item.toString()}
+          renderItem={({ item, index }) => (
+            <TouchableOpacity
+              onPress={() => {
+                setLoading(true);
+                api.order().updateOrder(order.id, { scheduledTo: Timestamp.fromDate(item) });
+                setLoading(false);
+                setSelectedSlot(Timestamp.fromDate(item));
+              }}
+            >
+              <View
+                style={{
+                  flexDirection: 'row',
+                  alignItems: 'center',
+                  justifyContent: 'space-between',
+                  marginTop: index !== 0 ? padding : 0,
+                }}
+              >
+                <Text style={{ ...texts.sm, color: colors.grey700, marginTop: halfPadding }}>
+                  {getETAWithMargin(item)}
+                </Text>
+                <CheckField
+                  checked={
+                    selectedSlot !== undefined && Timestamp.fromDate(item).isEqual(selectedSlot)
+                  }
+                />
+              </View>
+            </TouchableOpacity>
+          )}
+        />
+      </View>
+
+      {/* {realTimeDelivery ? (
         <View style={{ flex: 0.5 }}>
           <Text style={{ ...texts.md }}>{t('Entregar hoje')}</Text>
           <TouchableOpacity onPress={() => navigation.navigate('FoodOrderCheckout')}>
@@ -167,7 +251,7 @@ export const ScheduleOrder = ({ navigation, route }: Props) => {
         title={t('Confirmar')}
         onPress={() => navigation.navigate('FoodOrderCheckout')}
         activityIndicator={loading}
-      />
+      /> */}
     </View>
   );
 };
