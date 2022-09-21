49eb152a0ef39668a7d87b80904c96a744705e21
Daniel Lima <danielfbl@gmail.com>
2022-09-21 17:57:27 -0300

    minor

--- common/store/api/courier/account/useAdvanceByAmountSimulation.ts
+++ common/store/api/courier/account/useAdvanceByAmountSimulation.ts
@@ -4,13 +4,11 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 import { ApiContext } from '../../../../app/context';
 import { getCourier } from '../../../courier/selectors';
-import { useMarketplaceAccountInfo } from './useMarketplaceAccountInfo';
 
 export const useAdvanceByAmountSimulation = (amount?: number) => {
   // context
   const api = React.useContext(ApiContext);
   const navigation = useNavigation();
-  const info = useMarketplaceAccountInfo();
   // redux store
   const courier = useSelector(getCourier);
   const courierId = courier?.id;
@@ -23,7 +21,6 @@ export const useAdvanceByAmountSimulation = (amount?: number) => {
   };
   // side-effects
   React.useEffect(() => {
-    if (!info) return;
     if (!courierId) return;
     if (!amount) return;
     fetchSimulationByAmount();
