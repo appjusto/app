a4207e49a53bfb9932ac23cc69c594d6614846e3
Daniel Lima <danielfbl@gmail.com>
2022-11-02 13:27:29 -0300

    DeliveryHistoryCard created

---
+++ courier/approved/components/DeliveryHistoryCard.tsx
@@ -0,0 +1,82 @@
+import { formatDate } from '@appjusto/dates';
+import React from 'react';
+import { Text, TouchableOpacity, View } from 'react-native';
+import { useSelector } from 'react-redux';
+import ArrowBox from '../../../common/components/views/ArrowBox';
+import StatusBadge from '../../../common/components/views/StatusBadge';
+import { useDeliveryLedgerEntry } from '../../../common/store/api/courier/account/useDeliveryLedgerEntry';
+import { useObserveOrder } from '../../../common/store/api/order/hooks/useObserveOrder';
+import { getCourier } from '../../../common/store/courier/selectors';
+import { getOrderTime } from '../../../common/store/order/selectors';
+import { colors, halfPadding, padding, texts } from '../../../common/styles';
+import { formatCurrency, formatTime, separateWithDot } from '../../../common/utils/formatters';
+
+type Props = {
+  orderId: string;
+  onPress: () => void;
+};
+
+export const DeliveryHistoryCard = ({ orderId, onPress }: Props) => {
+  // redux store
+  const courier = useSelector(getCourier)!;
+  // screen state
+  const order = useObserveOrder(orderId);
+  const ledgerEntry = useDeliveryLedgerEntry(courier.id, orderId);
+  if (!order) return;
+  if (ledgerEntry === undefined) return;
+  // helpers
+  const time = getOrderTime(order);
+  const delivery =
+    (order.fare?.courier?.value ?? 0) - (order.fare?.courier?.processing?.value ?? 0);
+  const tip = (order.tip?.value ?? 0) - (order.tip?.processing?.value ?? 0);
+  const revenue = delivery + tip + (ledgerEntry?.value ?? 0);
+  const title = formatCurrency(revenue);
+  const subtitle = `Pedido ${order.code}\n${separateWithDot(formatDate(time), formatTime(time))}`;
+  // UI
+  return (
+    <TouchableOpacity onPress={onPress}>
+      <View
+        style={{
+          borderBottomColor: colors.grey500,
+          borderStyle: 'solid',
+          borderBottomWidth: 1,
+        }}
+      >
+        <View style={{ flex: 1 }}>
+          <Text style={{ ...texts.sm }}>{title}</Text>
+          <View
+            style={{
+              flexDirection: 'row',
+              justifyContent: 'space-between',
+              paddingVertical: padding,
+              paddingLeft: halfPadding,
+              paddingRight: padding,
+            }}
+          >
+            <View style={{ flex: 1 }}>
+              <Text
+                style={{
+                  ...texts.sm,
+                  color: colors.grey700,
+                  flexWrap: 'wrap',
+                }}
+              >
+                {subtitle}
+              </Text>
+            </View>
+            <View style={{ marginLeft: padding }}>
+              <ArrowBox />
+            </View>
+          </View>
+          {order ? (
+            <View
+              style={{ paddingRight: padding, paddingBottom: padding, paddingLeft: halfPadding }}
+            >
+              <StatusBadge order={order} />
+            </View>
+          ) : null}
+        </View>
+      </View>
+    </TouchableOpacity>
+  );
+};
