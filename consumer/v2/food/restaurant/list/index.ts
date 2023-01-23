116e9bf23e7c4505ac9c2c4b3d589f911594f4a8
Pedro Andrade <pdandradeb@gmail.com>
2023-01-23 11:16:19 -0300

    Updating restaurant list to show schedule only after open/realtime

--- consumer/v2/food/restaurant/list/index.ts
+++ consumer/v2/food/restaurant/list/index.ts
@@ -1,5 +1,6 @@
 import { Business, BusinessAlgolia, LatLng, WithId } from '@appjusto/types';
 import { uniqBy } from 'lodash';
+import { isAvailable } from '../../../../../common/store/api/business/selectors';
 import { distanceBetweenLatLng } from '../../../../../common/store/api/helpers';
 import { t } from '../../../../../strings';
 import { RestaurantListSection } from './types';
@@ -14,35 +15,46 @@ export const sectionsFromResults = (
   items: (BusinessAlgolia | WithId<Business>)[] = [],
   currentLocation?: LatLng | null
 ) => {
+  // abertos = dentro do horário de atendimento
+  // lista 1: "Disponíveis"
+  //          - (primeiro) restaurantes abertos que entregam realtime
+  //          - (depois dos realtime) restaurantes fechados que entregam agendados (Tag: abre às "13h")
+  // lista 2: "Indisponíveis"
+  //          - restaurantes fechados que não entregam agendados (Tag: abre às "13h")
+  //          - restaurantes fora da área de entrega
+  //          - restaurantes indisponíveis (unavailable)
   const open: (BusinessAlgolia | WithId<Business>)[] = [];
-  const openOutOfRange: (BusinessAlgolia | WithId<Business>)[] = [];
+  const scheduledOnly: (BusinessAlgolia | WithId<Business>)[] = [];
   const closed: (BusinessAlgolia | WithId<Business>)[] = [];
   uniqBy(items, (item) => ('id' in item ? item.id : item.objectID)).forEach((item) => {
-    if (item.status === 'available') {
-      if (!isOutOfRange(item, currentLocation)) open.push(item);
-      else openOutOfRange.push(item);
+    if (
+      item.status === 'available' &&
+      isAvailable(item.schedules, new Date()) &&
+      item.preparationModes?.includes('realtime')
+    ) {
+      open.push(item);
+    } else if (item.preparationModes?.includes('scheduled')) {
+      scheduledOnly.push(item);
     } else {
       closed.push(item);
     }
   });
+  open.push(...scheduledOnly);
   let sections: RestaurantListSection[] = [];
   if (open.length > 0) {
     sections = [
       {
         title: t('Restaurantes disponíveis'),
-        subtitle: t('Valor justo para restaurantes e entregadores/as'),
+        subtitle: t('Peça agora ou agende seu pedido'),
         data: open,
       },
     ];
-  }
-  if (openOutOfRange.length > 0) {
-    sections = [...sections, { data: openOutOfRange }];
   }
   if (closed.length > 0) {
     sections = [
       ...sections,
       {
-        title: t('Fechados no momento'),
+        title: t('Restaurantes indisponíveis'),
         subtitle: t('Fora do horário de funcionamento'),
         data: closed,
       },
