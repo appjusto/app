03e83f3678469859a9a80b851937901d76c600ed
Pedro Andrade <pdandradeb@gmail.com>
2023-01-23 12:04:39 -0300

    Updating version modal

--- common/components/views/UpgradeVersionModal.tsx
+++ common/components/views/UpgradeVersionModal.tsx
@@ -7,19 +7,43 @@ import { t } from '../../../strings';
 import { usePlatformAccess } from '../../hooks/usePlatformAccess';
 import { IconLogoGreen } from '../../icons/icon-logoGreen';
 import { colors, doublePadding, halfPadding, padding, texts } from '../../styles';
-import { getAppVersion, getNativeAndManifestVersion } from '../../utils/version';
+import { getAppVersion } from '../../utils/version';
 import DefaultButton from '../buttons/DefaultButton';
+
+type UpdateDetail = 'updated' | 'restart-update' | 'store-update';
 
 export const UpgradeVersionModal = (props: ModalProps) => {
   // context
   const platformAccess = usePlatformAccess();
+  // redux
   const flavor = useSelector(getFlavor);
-  const minVersion =
-    flavor === 'consumer'
-      ? platformAccess?.minVersions?.consumer
-      : flavor === 'courier'
-      ? platformAccess?.minVersions?.courier
-      : undefined;
+  // state
+  const [minVersion, setMinVersion] = React.useState<string>();
+  const [updateDetail, setUpdateDetail] = React.useState<UpdateDetail>();
+  const [modalVisible, setModalVisible] = React.useState(false);
+  // effects
+  React.useEffect(() => {
+    if (!platformAccess) return;
+    if (flavor === 'consumer' && platformAccess.minVersions?.consumer) {
+      setMinVersion(platformAccess.minVersions.consumer);
+    } else if (flavor === 'courier' && platformAccess.minVersions?.courier) {
+      setMinVersion(platformAccess.minVersions.courier);
+    }
+  }, [flavor, platformAccess]);
+  React.useEffect(() => {
+    if (!minVersion) return;
+    if (!compare(getAppVersion(), minVersion, '<')) setUpdateDetail('updated');
+    else {
+      const majorCurrent = getAppVersion().split('.')[0];
+      const majorMin = minVersion.split('.')[0];
+      if (majorCurrent === majorMin) setUpdateDetail('restart-update');
+      else setUpdateDetail('store-update');
+    }
+  }, [minVersion]);
+  React.useEffect(() => {
+    setModalVisible(updateDetail === 'restart-update' || updateDetail === 'store-update');
+  }, [updateDetail]);
+  // handlers
   const onUpgradeHandler = () => {
     if (flavor === 'consumer') {
       if (Platform.OS === 'android') {
@@ -34,13 +58,6 @@ export const UpgradeVersionModal = (props: ModalProps) => {
         'https://play.google.com/store/apps/details?id=br.com.appjusto.consumer.live&hl=pt_BR&gl=US'
       );
   };
-  // screen state
-  const [modalVisible, setModalVisible] = React.useState(false);
-  // effects
-  React.useEffect(() => {
-    if (!minVersion) return;
-    setModalVisible(compare(getAppVersion(), minVersion, '<'));
-  }, [minVersion]);
   // UI
   return (
     <Modal transparent {...props} visible={modalVisible}>
@@ -66,22 +83,27 @@ export const UpgradeVersionModal = (props: ModalProps) => {
             </Pressable>
           </View>
           <View style={{ marginBottom: padding }}>
-            <Text style={{ ...texts.md }}>
-              {t('Você está utilizando uma versão antiga do AppJusto - ')}
-              <Text style={{ color: colors.red, textDecorationLine: 'underline' }}>
-                {t('versão:')} {getNativeAndManifestVersion()}
-              </Text>
-              {t(
-                '. Atualizações são importantes, pois mantém o aplicativo funcionando da melhor maneira para os seus pedidos.'
-              )}
+            <Text style={{ ...texts.x2l }}>Atualização necessária</Text>
+            <Text style={{ ...texts.md, marginTop: padding }}>
+              {updateDetail === 'restart-update'
+                ? t('Para continuar utilizando o AppJusto, feche o aplicativo e o abra novamente.')
+                : null}
+              {updateDetail === 'store-update'
+                ? t('Para continuar utilizando o AppJusto, baixe a versão mais nova na loja.')
+                : null}
             </Text>
             <Text style={{ ...texts.md, marginTop: 24 }}>
-              {t('Por favor, atualize seu aplicativo para continuar usando.')}
+              {t('Versão mínima:')} {minVersion}
+            </Text>
+            <Text style={{ ...texts.md, marginTop: halfPadding }}>
+              {t('Versão atual:')} {getAppVersion()}
             </Text>
           </View>
-          <View>
-            <DefaultButton title={t('Atualizar agora')} onPress={onUpgradeHandler} />
-          </View>
+          {updateDetail === 'store-update' ? (
+            <View>
+              <DefaultButton title={t('Atualizar agora')} onPress={onUpgradeHandler} />
+            </View>
+          ) : null}
         </View>
       </View>
     </Modal>
