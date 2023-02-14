c6efa3f00b227d8f35b82b160b905b699768570f
Pedro Andrade <pdandradeb@gmail.com>
2023-02-14 08:34:58 -0300

    Fixing order revenue with new format

--- common/store/api/order/courier/getOrderRevenue.ts
+++ common/store/api/order/courier/getOrderRevenue.ts
@@ -4,9 +4,8 @@ export const getOrderRevenue = (order: Order) => {
   let value = 0;
   if (!order.fare?.courier) value = 0;
   else if (order.fare.courier.paid) value = order.fare.courier.paid;
-  else if (order.fare.courier.netValue) value = order.fare.courier.netValue;
-  else {
-    value = order.fare.courier.value - (order.fare.courier.processing?.value ?? 0);
+  else if (order.fare.courier.netValue) {
+    value = order.fare.courier.netValue + (order.fare.courier.locationFee ?? 0);
   }
   if (order.tip?.value && order.tip.status === 'paid') {
     value += order.tip.value - (order.tip?.processing?.value ?? 0);
