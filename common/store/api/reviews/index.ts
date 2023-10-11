2d3f6db8820bd4315b5f8f5d6de258e3dbd8e36e
Pedro Andrade <pdandradeb@gmail.com>
2023-10-11 12:57:22 -0300

    Fixing review

--- common/store/api/reviews/index.ts
+++ common/store/api/reviews/index.ts
@@ -1,7 +1,8 @@
 import { Flavor, OrderConsumerReview, Review, ReviewTag, WithId } from '@appjusto/types';
-import { doc, getDocs, limit, query, serverTimestamp, setDoc, where } from 'firebase/firestore';
+import { doc, getDocs, query, serverTimestamp, setDoc, where } from 'firebase/firestore';
+import { OrderReview } from '../../../../../types';
 import { FirestoreRefs } from '../../refs/FirestoreRefs';
-import { documentAs, documentsAs } from '../types';
+import { documentsAs } from '../types';
 
 export default class ReviewsApi {
   constructor(private firestoreRefs: FirestoreRefs) {}
@@ -17,15 +18,14 @@ export default class ReviewsApi {
   }
   async fetchOrderReview(orderId: string, consumerId: string) {
     const snapshot = await getDocs(
-      query(
-        this.firestoreRefs.getReviewsRef(),
-        where('orderId', '==', orderId),
-        where('consumer.id', '==', consumerId),
-        limit(1)
-      )
+      query(this.firestoreRefs.getReviewsRef(), where('orderId', '==', orderId))
     );
     if (snapshot.empty) return null;
-    return documentAs<OrderConsumerReview>(snapshot.docs.find(() => true)!);
+    const reviews = documentsAs<OrderReview>(snapshot.docs);
+    let review = reviews.find((review) => review.createdBy?.id === consumerId);
+    if (review) return review;
+    review = reviews.find((review) => review.consumer?.id === consumerId && !review.createdBy);
+    return review;
   }
 
   async fetchReviewTags(agent: Flavor, type: string) {
